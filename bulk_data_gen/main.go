1f9f9288aa9d0c75dba8194bd0c31b7609f695dd
rw <me@rwinslow.com>
2016-04-11 15:36:18 -0700

    Separate out the bulk_data_gen.

---
+++ bulk_data_gen/main.go
@@ -0,0 +1,201 @@
+// bulk_data_gen generates time series data.
+//
+// Supported formats are:
+// InfluxDB bulk load format
+// ElasticSearch bulk load format
+//
+// The options provide ways to change the following parameters:
+//   # total points to write
+//   # measurements to spread points across
+//   # tag key/value pairs to spread points across (per measurement)
+//   standard deviations and means of the field values
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+// Output data format choices:
+var formatChoices = []string{"influx-bulk", "es-bulk"}
+
+// Program option vars:
+var (
+	daemonUrl string
+	dbName    string
+
+	measurements uint
+	fields       uint
+	// TODO(rw): valueStrategy: generate from other distributions?
+	batchSize uint
+	tagKeys   uint
+	tagValues uint
+	points    uint
+	// TODO(rw): precision?
+
+	measurementNameLen uint
+	fieldNameLen       uint
+	tagKeyLen          uint
+	tagValueLen        uint
+
+	stdDevsStr string
+	meansStr   string
+
+	stdDevs []float64
+	means   []float64
+
+	seed  int64
+	debug int
+
+	format string
+)
+
+// Parse args:
+func init() {
+	flag.StringVar(&format, "format", "influx-bulk", "Format to emit. (choices: influx-bulk, es-bulk)")
+
+	flag.UintVar(&measurements, "measurements", 1, "Number of measurements to create.")
+	flag.UintVar(&fields, "fields", 1, "Number of fields to populate per point.")
+	flag.UintVar(&batchSize, "batch-size", 1000, "Number of points to write per request.")
+	flag.UintVar(&tagKeys, "tag-keys", 1, "Number of tag keys to generate per point. These are generated per measurement.")
+	flag.UintVar(&tagValues, "tag-values", 1, "Number of tag values to generate per tag key. These are generated per measurement.")
+	flag.UintVar(&points, "points", 10000, "Number of points to generate. Points are split evenly across all measurements.")
+
+	flag.UintVar(&measurementNameLen, "measurement-name-len", 5, "Length of generated measurement names.")
+	flag.UintVar(&fieldNameLen, "field-name-len", 5, "Length of generated field names.")
+	flag.UintVar(&tagKeyLen, "tag-key-len", 5, "Length of generated tag keys.")
+	flag.UintVar(&tagValueLen, "tag-value-len", 5, "Length of generated tag values.")
+
+	flag.StringVar(&stdDevsStr, "std-devs", "1.0", "Comma-separated std deviations for generating field values. Will be repeated to satisfy all generators. Example: 1.0,2.0,3.0")
+	flag.StringVar(&meansStr, "means", "0.0", "Comma-separated means for generating field values. Will be repeated to satisfy all generators. Example: 0.0,1.0,2.0")
+
+	flag.Int64Var(&seed, "seed", 0, "PRNG seed (default, or 0, uses the current timestamp).")
+	flag.IntVar(&debug, "debug", 0, "Debug printing (choices: 0, 1, 2) (default 0).")
+
+	flag.Parse()
+
+	validFormat := false
+	for _, s := range formatChoices {
+		if s == format {
+			validFormat = true
+			break
+		}
+	}
+	if !validFormat {
+		log.Fatal("invalid format specifier")
+	}
+
+	// the default seed is the current timestamp:
+	if seed == 0 {
+		seed = int64(time.Now().Nanosecond())
+	}
+
+	// Parse std dev numbers, cycling the slice to fill up `fields` items:
+	stdDevNums := make([]float64, 0)
+	for _, s := range strings.Split(stdDevsStr, ",") {
+		var n float64
+		if _, err := fmt.Sscanf(s, "%f", &n); err != nil {
+			log.Fatalf("std-devs parsing failure: %s", err)
+		}
+		stdDevNums = append(stdDevNums, n)
+	}
+
+	stdDevs = make([]float64, int(fields))
+	for i := 0; i < int(fields); i++ {
+		stdDevs[i] = stdDevNums[i%len(stdDevNums)]
+	}
+
+	// Parse mean numbers, cycling the slice to fill up `fields` items:
+	meanNums := make([]float64, 0)
+	for _, s := range strings.Split(meansStr, ",") {
+		var n float64
+		if _, err := fmt.Sscanf(s, "%f", &n); err != nil {
+			log.Fatalf("means parsing failure: %s", err)
+		}
+		meanNums = append(meanNums, n)
+	}
+	means = make([]float64, int(fields))
+	for i := 0; i < int(fields); i++ {
+		means[i] = meanNums[i%len(meanNums)]
+	}
+
+	if debug >= 1 {
+		fmt.Printf("stddevs: %v\n", stdDevs)
+		fmt.Printf("means: %v\n", means)
+	}
+}
+
+func main() {
+	rand.Seed(seed)
+
+	out := bufio.NewWriterSize(os.Stdout, 1<<20)
+	defer out.Flush()
+
+	// Initialize generators, one for each measurement:
+	generators := make([]*MeasurementGenerator, measurements)
+	for i := 0; i < len(generators); i++ {
+		config := MeasurementGeneratorConfig{
+			NameLen: int(measurementNameLen),
+
+			TagKeyCount:   int(tagKeys),
+			TagKeyLen:     int(tagKeyLen),
+			TagValueCount: int(tagValues),
+			TagValueLen:   int(tagValueLen),
+
+			FieldCount:   int(fields),
+			FieldNameLen: int(fieldNameLen),
+			FieldStdDevs: stdDevs,
+			FieldMeans:   means,
+		}
+		if err := config.Validate(); err != nil {
+			log.Fatal(err)
+		}
+
+		g := NewMeasurementGenerator(&config)
+		generators[i] = &g
+	}
+
+	generatorIdx := 0
+	bytesWritten := int64(0)
+	for i := int64(0); i < int64(points); i++ {
+		// Construct the next point and write it to the buffer:
+		g := generators[generatorIdx]
+
+		switch format {
+		case "influx-bulk":
+			err := g.P.SerializeInfluxBulk(out)
+			if err != nil {
+				log.Fatal(err)
+			}
+		case "es-bulk":
+			err := g.P.SerializeESBulk(out)
+			if err != nil {
+				log.Fatal(err)
+			}
+		default: panic("unreachable") 
+		}
+
+		// increment and wrap around the generator index:
+		generatorIdx++
+		generatorIdx %= len(generators)
+		if generatorIdx == 0 {
+			for _, g := range generators {
+				g.Next()
+			}
+		}
+	}
+	fmt.Printf("created %d points across %d measurements (%.2fMB)\n", points,
+		len(generators), float64(bytesWritten)/(1<<20))
+	for _, g := range generators {
+		fmt.Printf("  %s: %d points. tag pairs: %d, fields: %d, stddevs: %v, means: %v\n",
+			g.Name, g.Count,
+			g.Config.TagKeyCount*g.Config.TagValueCount,
+			g.Config.FieldCount, g.FieldStdDevs, g.FieldMeans)
+	}
+}
---
+++ bulk_data_gen/measurement_generator.go
@@ -0,0 +1,330 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+)
+
+// Used for generating random field names:
+const letters = "abcdefghijklmnopqrstuvwxyz"
+
+// MeasurementGeneratorConfig stores parameters used to construct a
+// MeasurementGenerator.
+type MeasurementGeneratorConfig struct {
+	NameLen int
+
+	TagKeyLen   int
+	TagKeyCount int
+
+	TagValueLen   int
+	TagValueCount int
+
+	FieldNameLen int
+	FieldCount   int
+	FieldStdDevs []float64
+	FieldMeans   []float64
+}
+
+// Validate checks that a MeasurementGeneratorConfig is sane.
+func (cfg *MeasurementGeneratorConfig) Validate() error {
+	if cfg.NameLen == 0 {
+		return fmt.Errorf("empty name")
+	}
+
+	if cfg.TagKeyCount > 0 && cfg.TagValueCount == 0 {
+		return fmt.Errorf("tag value count is too small")
+	}
+
+	if cfg.TagValueLen == 0 && cfg.TagValueCount > 0 {
+		return fmt.Errorf("tag value len is too small")
+	}
+
+	if cfg.TagKeyLen == 0 && cfg.TagKeyCount > 0 {
+		return fmt.Errorf("tag key len is too small")
+	}
+
+	if cfg.FieldNameLen == 0 && cfg.FieldCount > 0 {
+		return fmt.Errorf("field name len is too small")
+	}
+
+	if len(cfg.FieldStdDevs) != cfg.FieldCount {
+		return fmt.Errorf("field std deviations slice is the wrong size")
+	}
+
+	if len(cfg.FieldStdDevs) != cfg.FieldCount {
+		return fmt.Errorf("field means slice is the wrong size")
+	}
+
+	return nil
+}
+
+// Point wraps a single data point. It is currently only used by
+// MeasurementGenerator instances.
+//
+// Internally, Point stores byte slices, instead of strings, to minimize
+// overhead.
+type Point struct {
+	MeasurementName []byte
+	TagKeys         [][]byte
+	TagValues       [][]byte
+	FieldKeys       [][]byte
+	FieldValues     []float64
+	Timestamp       uint64
+}
+
+// Using these literals prevents the slices from escaping to the heap, saving
+// a few micros per call:
+var (
+	charComma  = []byte(",")
+	charEquals = []byte("=")
+	charSpace  = []byte(" ")
+)
+
+// SerializeInfluxBulk writes Point data to the given writer, conforming to the
+// InfluxDB wire protocol.
+//
+// This function writes output that looks like:
+// <measurement>,<tag key>=<tag value> <field name>=<field value> <timestamp>\n
+//
+// For example:
+// foo,tag0=bar baz=-1.0 100\n
+func (p *Point) SerializeInfluxBulk(w io.Writer) error {
+	_, err := w.Write(p.MeasurementName)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(p.TagKeys); i++ {
+		_, err = w.Write(charComma)
+		if err != nil {
+			return err
+		}
+
+		_, err = w.Write(p.TagKeys[i])
+		if err != nil {
+			return err
+		}
+		_, err = w.Write(charEquals)
+		if err != nil {
+			return err
+		}
+
+		_, err = w.Write(p.TagValues[i])
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(p.FieldKeys) > 0 {
+		_, err = w.Write(charSpace)
+		if err != nil {
+			return err
+		}
+	}
+
+	for i := 0; i < len(p.FieldKeys); i++ {
+		_, err = w.Write(p.FieldKeys[i])
+		if err != nil {
+			return err
+		}
+
+		_, err = fmt.Fprintf(w, "=%f", p.FieldValues[i])
+		if err != nil {
+			return err
+		}
+
+		if i+1 < len(p.FieldKeys) {
+			_, err = w.Write(charComma)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+
+	_, err = fmt.Fprintf(w, " %d\n", p.Timestamp)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+// SerializeESBulk writes Point data to the given writer, conforming to the
+// ElasticSearch bulk load protocol.
+//
+// This function writes output that looks like:
+// <action line>
+// <tags, fields, and timestamp>
+//
+// For example:
+// { "create" : { "_index" : "measurement_otqio", "_type" : "point" } }\n
+// { "tag_launx": "btkuw", "tag_gaijk": "jiypr", "field_wokxf": 0.08463898963964356, "field_zqstf": -0.043641533500086316, "timestamp": 171300 }\n
+func (p *Point) SerializeESBulk(w io.Writer) error {
+	action := "{ \"create\" : { \"_index\" : \"%s\", \"_type\" : \"point\" } }\n"
+	_, err := fmt.Fprintf(w, action, p.MeasurementName)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write([]byte("{ "))
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(p.TagKeys); i++ {
+		if i > 0 {
+			_, err = fmt.Fprintf(w, ", ")
+			if err != nil {
+				return err
+			}
+		}
+		_, err = fmt.Fprintf(w, "\"%s\": ", p.TagKeys[i])
+		if err != nil {
+			return err
+		}
+
+		_, err = fmt.Fprintf(w, "\"%s\"", p.TagValues[i])
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(p.TagKeys) > 0 && len(p.FieldKeys) > 0 {
+		_, err = fmt.Fprintf(w, ", ")
+		if err != nil {
+			return err
+		}
+	}
+
+	for i := 0; i < len(p.FieldKeys); i++ {
+		if i > 0 {
+			_, err = fmt.Fprintf(w, ", ")
+			if err != nil {
+				return err
+			}
+		}
+		_, err = fmt.Fprintf(w, "\"%s\": ", p.FieldKeys[i])
+		if err != nil {
+			return err
+		}
+
+		_, err = fmt.Fprintf(w, "%#v", p.FieldValues[i])
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(p.TagKeys) > 0 || len(p.FieldKeys) > 0 {
+		_, err = fmt.Fprintf(w, ", ")
+		if err != nil {
+			return err
+		}
+	}
+	_, err = fmt.Fprintf(w, "\"timestamp\": %d }\n", p.Timestamp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// MeasurementGenerator creates data for populating a given measurement.
+// Instances are created using a MeasurementGeneratorConfig.
+//
+// Field values are sampled from a normal distribution.
+type MeasurementGenerator struct {
+	Name            []byte
+	TagKeys         [][]byte
+	TagValueChoices [][][]byte
+	FieldKeys       [][]byte
+
+	P            *Point
+	FieldMeans   []float64
+	FieldStdDevs []float64
+
+	Count int64
+
+	Config *MeasurementGeneratorConfig
+}
+
+// NewMeasurementGenerator creates a MeasurementGenerator from the given
+// MeasurementGeneratorConfig.
+func NewMeasurementGenerator(cfg *MeasurementGeneratorConfig) MeasurementGenerator {
+	tvc := make([][][]byte, cfg.TagKeyCount)
+	for i := 0; i < len(tvc); i++ {
+		tvc[i] = randBytesSeq(nil, cfg.TagValueCount, cfg.TagValueLen)
+	}
+	g := MeasurementGenerator{
+		Name:            randBytes([]byte("measurement_"), cfg.NameLen),
+		TagKeys:         randBytesSeq([]byte("tag_"), cfg.TagKeyCount, cfg.TagKeyLen),
+		TagValueChoices: tvc,
+		FieldKeys:       randBytesSeq([]byte("field_"), cfg.FieldCount, cfg.FieldNameLen),
+
+		P:            &Point{},
+		FieldMeans:   cfg.FieldMeans,
+		FieldStdDevs: cfg.FieldStdDevs,
+
+		Config: cfg,
+	}
+	g.initPoint()
+	return g
+}
+
+// initPoint initializes bookkeeping for a MeasurementGenerator. It should be
+// called exactly once.
+func (mg *MeasurementGenerator) initPoint() {
+	mg.P.MeasurementName = []byte(mg.Name)
+
+	mg.P.TagKeys = mg.TagKeys
+	mg.P.TagValues = make([][]byte, len(mg.TagKeys))
+
+	mg.P.FieldKeys = mg.FieldKeys
+	mg.P.FieldValues = make([]float64, len(mg.FieldKeys))
+
+	mg.P.Timestamp = 0
+}
+
+// Next updates the internal Point object with new data.
+func (mg *MeasurementGenerator) Next() {
+	// choose tag values
+	for i := 0; i < len(mg.TagKeys); i++ {
+		idx := rand.Int63n(int64(len(mg.TagValueChoices[i])))
+		v := mg.TagValueChoices[i][idx]
+		mg.P.TagValues[i] = v
+	}
+
+	// choose field values
+	for i := 0; i < len(mg.FieldKeys); i++ {
+		v := rand.NormFloat64()*mg.FieldStdDevs[i] + mg.FieldMeans[i]
+		mg.P.FieldValues[i] = v
+	}
+	mg.P.Timestamp += 100 // ns
+
+	mg.Count++
+}
+
+// randBytes creates random bytes of the given length. If the prefix is
+// non-empty, it will be prepended to the result.
+func randBytes(prefix []byte, length int) []byte {
+	b := make([]byte, 0, length+len(prefix))
+	if len(prefix) > 0 {
+		b = append(prefix, b...)
+	}
+	for i := 0; i < length; i++ {
+		c := letters[rand.Intn(len(letters))]
+		b = append(b, c)
+	}
+	return b
+}
+
+// randBytesSeq creates a nested slice of `count` byte slices, each element
+// having `length` random bytes. If the prefix is non-empty, it will be
+// prepended to each element.
+func randBytesSeq(prefix []byte, count, length int) [][]byte {
+	bb := make([][]byte, count)
+	for i := 0; i < count; i++ {
+		bb[i] = randBytes(prefix, length)
+	}
+	return bb
+}
