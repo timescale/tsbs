5025cc408bee19816693fbfd351fb7150bebe361
rw <me@rwinslow.com>
2016-04-12 15:39:42 -0700

    Query scripts take date parameters.

--- benchmark_influx_queries.sh
+++ benchmark_influx_queries.sh
@@ -3,5 +3,9 @@ set -euf -o pipefail
 count=${1}
 measurement=${2}
 field=${3}
+start_date=${4}
+end_date=${5}
+interval=${6}
+
 echo "${count} queries:"
-time (for i in `seq ${count}`; do ./influx_query.sh ${measurement} ${field} > /dev/null; done 2>/dev/null)
+time (for i in `seq ${count}`; do ./influx_query.sh ${measurement} ${field} ${start_date} ${end_date} ${interval} >/dev/null; done 2>/dev/null)
