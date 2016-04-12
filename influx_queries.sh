9a618f6f03065cc47ab27e0af3d0ab1aab860ccc
rw <me@rwinslow.com>
2016-04-12 13:55:38 -0700

    Add time grouping example to Influx queries.

--- influx_queries.sh
+++ influx_queries.sh
@@ -7,3 +7,7 @@ field=${3}
 query="q=SELECT count(${field}), min(${field}), max(${field}), mean(${field}), sum(${field}) FROM ${measurement}"
 echo $query
 curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=${database}" --data-urlencode "${query}"
+
+query="q=SELECT count(${field}) FROM ${measurement} WHERE time >= '2016-01-01' GROUP BY time(1w)"
+echo $query
+curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=${database}" --data-urlencode "${query}"
