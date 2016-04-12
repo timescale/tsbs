ea753e4cab458d49a02c00799bcb93c4a1eee681
rw <me@rwinslow.com>
2016-04-12 14:56:14 -0700

    Update query scripts (with range parameters).

--- es_query.sh
+++ es_query.sh
@@ -2,14 +2,40 @@
 set -euf -o pipefail
 measurement=${1}
 field=${2}
+#query="
+#{
+#  \"size\": 0,
+#  \"aggs\": {
+#        \"my_stats\" : { \"stats\" : { \"field\" : \"${field}\" } }
+#  }
+#}
+#"
+#
+#echo $query
+#echo $query | curl -XPOST "localhost:9200/${measurement}/transactions/_search?pretty" -d @-
+
 query="
 {
-  \"size\": 0,
-  \"aggs\": {
-        \"my_stats\" : { \"stats\" : { \"field\" : \"${field}\" } }
-  }
+   \"size\" : 0,
+   \"filter\": {
+     \"range\": {
+       \"timestamp\": {
+         \"gte\": \"2016-01-01\",
+         \"lt\": \"2016-01-15\"
+       }
+     }
+   },
+   \"aggs\": {
+      \"result\": {
+         \"date_histogram\": {
+            \"field\": \"timestamp\",
+            \"interval\": \"1h\",
+            \"format\": \"yyyy-MM-dd-HH\"
+         }
+      }
+   }
 }
 "
-
 echo $query
-echo $query | curl -XPOST "localhost:9200/${measurement}/_search?pretty" -d @-
+#echo $query | curl -G "localhost:9200/${measurement}/_search?pretty" -d @-
+echo $query | curl "localhost:9200/${measurement}/_search?pretty" -d @-
--- influx_query.sh
+++ influx_query.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 set -euf -o pipefail
-database=${1}
-measurement=${2}
-field=${3}
+database="benchmark_db"
+measurement=${1}
+field=${2}
 
-query="q=SELECT count(${field}), min(${field}), max(${field}), mean(${field}), sum(${field}) FROM ${measurement}"
-echo $query
-curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=${database}" --data-urlencode "${query}"
+#query="q=SELECT count(${field}), min(${field}), max(${field}), mean(${field}), sum(${field}) FROM ${measurement}"
+#echo $query
+#curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=${database}" --data-urlencode "${query}"
 
-query="q=SELECT count(${field}) FROM ${measurement} WHERE time >= '2016-01-01' GROUP BY time(1w)"
-echo $query
+query="q=SELECT count(${field}) FROM ${measurement} WHERE time >= '2016-01-01' AND time < '2016-01-15' GROUP BY time(1h)"
+#echo $query
 curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode "db=${database}" --data-urlencode "${query}"
