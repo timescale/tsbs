b6f411d98024a0e84eec07c7dbc6a8f2456d8002
Erik Nordström <erik.nordstrom@gmail.com>
2016-09-09 15:23:43 +0200

    Link generated data files to simple name

--- utils/generate_data.sh
+++ utils/generate_data.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-binName="./bulk_data_gen"
+binName=$(which bulk_data_gen)
 binVersion=`md5sum $binName | awk '{ print $1 }'`
-dataDir="datafiles"
-
+dataDir=${dataDir:-datafiles}
 
 declare -a formats=("influx-bulk" "cassandra" "iobeamdb")
 logSeconds="10s"
@@ -13,12 +12,15 @@ tsStart="2016-01-01T00:00:00Z"
 tsEnd="2016-01-01T00:10:00Z"
 useCase="devops"
 
+mkdir -p ${dataDir}
+
 for format in "${formats[@]}"
 do
-    fname="${dataDir}/import_${format}_${binVersion}_${logSeconds}_${scaleVar}_${seed}_${tsStart}_${tsEnd}_${useCase}.dat"
+    fname="${dataDir}/import_${format}_${binVersion}_${logSeconds}_${scaleVar}_${seed}_${tsStart}_${tsEnd}_${useCase}.dat.gz"
     echo "$fname"
     if [ ! -f "$fname" ]; then
-        $binName -format $format -logSeconds $logSeconds -scale-var $scaleVar -seed $seed -timestamp-end $tsEnd -timestamp-start $tsStart -use-case $useCase > $fname
+        $binName -format $format -logSeconds $logSeconds -scale-var $scaleVar -seed $seed -timestamp-end $tsEnd -timestamp-start $tsStart -use-case $useCase | gzip > $fname
+        ln -s $fname ${dataDir}/${format}.gz
     fi
    # or do whatever with individual element of the array
 done
