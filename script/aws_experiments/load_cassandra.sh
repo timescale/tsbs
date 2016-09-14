d153f9c3a132cae46995707d6474027a48b3a38e
Olof Rensfelt <olof@iobeam.com>
2016-09-14 15:49:03 +0200

    reduce cassandra batch size

--- script/aws_experiments/load_cassandra.sh
+++ script/aws_experiments/load_cassandra.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+set -x
 EXE_DIR=${EXE_DIR:-$(dirname $0)}
 DATA_FILE_NAME=${DATA_FILE_NAME:-cassandra.gz}
+CASSANDRA_BATCH_SIZE=${CASSANDRA_BATCH_SIZE:-100}
 source ${EXE_DIR}/load_common.sh
 
 cat ${DATA_FILE} | gunzip | bulk_load_cassandra \
                                 --workers=${NUM_WORKERS} \
-                                --batch-size=${BATCH_SIZE} \
+                                --batch-size=${CASSANDRA_BATCH_SIZE} \
                                 --url=${DATABASE_HOST}:9042 
