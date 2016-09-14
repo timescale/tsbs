641720b7647f715420bdb91be8f54c694ba4c549
Olof Rensfelt <olof@iobeam.com>
2016-09-14 16:25:19 +0200

    add cassandra write timeout

--- script/aws_experiments/load_cassandra.sh
+++ script/aws_experiments/load_cassandra.sh
@@ -3,9 +3,11 @@ set -x
 EXE_DIR=${EXE_DIR:-$(dirname $0)}
 DATA_FILE_NAME=${DATA_FILE_NAME:-cassandra.gz}
 CASSANDRA_BATCH_SIZE=${CASSANDRA_BATCH_SIZE:-100}
+CASSANDRA_TIMEOUT=${CASSANDRA_TIMEOUT:-1000s}
 source ${EXE_DIR}/load_common.sh
 
 cat ${DATA_FILE} | gunzip | bulk_load_cassandra \
                                 --workers=${NUM_WORKERS} \
                                 --batch-size=${CASSANDRA_BATCH_SIZE} \
+                                --write-timeout=${CASSANDRA_TIMEOUT} \
                                 --url=${DATABASE_HOST}:9042 
