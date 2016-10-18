ebf1c625261d6b40f14841de913201ab1282aa02
rw <me@rwinslow.com>
2016-10-18 16:46:36 -0700

    wip opentsdb loader

---
+++ bulk_load_opentsdb/main.go
@@ -0,0 +1,216 @@
+// bulk_load_opentsdb loads an OpenTSDB daemon with data from stdin.
+//
+// The caller is responsible for assuring that the database is empty before
+// bulk load.
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/pkg/profile"
+)
+
+// Program option vars:
+var (
+	daemonUrl string
+	workers   int
+	batchSize int
+	backoff   time.Duration
+	doLoad    bool
+	memprofile bool
+)
+
+// Global vars
+var (
+	bufPool        sync.Pool
+	batchChan      chan *bytes.Buffer
+	inputDone      chan struct{}
+	workersGroup   sync.WaitGroup
+	backingOffChan chan bool
+	backingOffDone chan struct{}
+)
+
+// Parse args:
+func init() {
+	flag.StringVar(&daemonUrl, "url", "http://localhost:8086", "OpenTSDB URL.")
+	flag.IntVar(&batchSize, "batch-size", 5000, "Batch size (input lines).")
+	flag.IntVar(&workers, "workers", 1, "Number of parallel requests to make.")
+	//flag.DurationVar(&backoff, "backoff", time.Second, "Time to sleep between requests when server indicates backpressure is needed.")
+	flag.BoolVar(&doLoad, "do-load", true, "Whether to write data. Set this flag to false to check input read speed.")
+	flag.BoolVar(&memprofile, "memprofile", false, "Whether to write a memprofile (file automatically determined).")
+
+	flag.Parse()
+}
+
+func main() {
+	if memprofile {
+		p := profile.Start(profile.MemProfile)
+		defer p.Stop()
+	}
+	if doLoad {
+		// check that there are no pre-existing databases:
+		existingDatabases, err := listDatabases(daemonUrl)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(existingDatabases) > 0 {
+			log.Fatalf("There are databases already in the data store. If you know what you are doing, run the command:\ncurl 'http://localhost:8086/query?q=drop%%20database%%20%s'\n", existingDatabases[0])
+		}
+	}
+
+	bufPool = sync.Pool{
+		New: func() interface{} {
+			return bytes.NewBuffer(make([]byte, 0, 4*1024*1024))
+		},
+	}
+
+	batchChan = make(chan *bytes.Buffer, workers)
+	inputDone = make(chan struct{})
+
+	backingOffChan = make(chan bool, 100)
+	backingOffDone = make(chan struct{})
+
+	for i := 0; i < workers; i++ {
+		workersGroup.Add(1)
+		cfg := HTTPWriterConfig{
+			Host:     daemonUrl,
+		}
+		go processBatches(NewHTTPWriter(cfg))
+	}
+
+	go processBackoffMessages()
+
+	start := time.Now()
+	itemsRead := scan(batchSize)
+
+	<-inputDone
+	close(batchChan)
+
+	workersGroup.Wait()
+
+	close(backingOffChan)
+	<-backingOffDone
+
+	end := time.Now()
+	took := end.Sub(start)
+	rate := float64(itemsRead) / float64(took.Seconds())
+
+	fmt.Printf("loaded %d items in %fsec with %d workers (mean rate %f/sec)\n", itemsRead, took.Seconds(), workers, rate)
+}
+
+// scan reads one line at a time from stdin.
+// When the requested number of lines per batch is met, send a batch over batchChan for the workers to write.
+func scan(linesPerBatch int) int64 {
+	buf := bufPool.Get().(*bytes.Buffer)
+
+	var n int
+	var itemsRead int64
+
+	openbracket := []byte("[")
+	closebracket := []byte("]")
+	commaspace := []byte(", ")
+	newline := []byte("\n")
+
+	buf.Write(openbracket)
+	buf.Write(newline)
+
+	scanner := bufio.NewScanner(bufio.NewReaderSize(os.Stdin, 4*1024*1024))
+	for scanner.Scan() {
+		itemsRead++
+
+		buf.Write(scanner.Bytes())
+
+		n++
+		if n < linesPerBatch {
+			buf.Write(commaspace)
+			buf.Write(newline)
+		} else {
+			buf.Write(newline)
+			buf.Write(closebracket)
+
+			batchChan <- buf
+
+			buf = bufPool.Get().(*bytes.Buffer)
+			buf.Write(openbracket)
+			buf.Write(newline)
+			n = 0
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading input: %s", err.Error())
+	}
+
+	// Finished reading input, make sure last batch goes out.
+	if n > 0 {
+		buf.Write(closebracket)
+		batchChan <- buf
+	}
+
+	// Closing inputDone signals to the application that we've read everything and can now shut down.
+	close(inputDone)
+
+	return itemsRead
+}
+
+// processBatches reads byte buffers from batchChan and writes them to the target server, while tracking stats on the write.
+func processBatches(w LineProtocolWriter) {
+	for batch := range batchChan {
+		// Write the batch: try until backoff is not needed.
+		if doLoad {
+			var err error
+			for {
+				_, err = w.WriteLineProtocol(batch.Bytes())
+				if err == BackoffError {
+					backingOffChan <- true
+					time.Sleep(backoff)
+				} else {
+					backingOffChan <- false
+					break
+				}
+			}
+			if err != nil {
+				log.Fatalf("Error writing: %s\n", err.Error())
+			}
+		}
+		//fmt.Println(string(batch.Bytes()))
+
+		// Return the batch buffer to the pool.
+		batch.Reset()
+		bufPool.Put(batch)
+	}
+	workersGroup.Done()
+}
+
+func processBackoffMessages() {
+	var totalBackoffSecs float64
+	var start time.Time
+	last := false
+	for this := range backingOffChan {
+		if this && !last {
+			start = time.Now()
+			last = true
+		} else if !this && last {
+			took := time.Now().Sub(start)
+			fmt.Printf("backoff took %.02fsec\n", took.Seconds())
+			totalBackoffSecs += took.Seconds()
+			last = false
+			start = time.Now()
+		}
+	}
+	fmt.Printf("backoffs took a total of %fsec of runtime\n", totalBackoffSecs)
+	backingOffDone<-struct{}{}
+}
+
+// TODO(rw): listDatabases lists the existing data in OpenTSDB.
+func listDatabases(daemonUrl string) ([]string, error) {
+	return nil, nil
+}
