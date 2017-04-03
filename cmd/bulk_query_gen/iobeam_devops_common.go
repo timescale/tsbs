22bdd21355e5d2c95a236ddfd1a1de29acfb5d72
Rob Kiefer <rob.kiefer@gmail.com>
2017-04-03 16:30:26 -0400

    Switch TimescaleDB (formerly iobeam) to use timestamps instead of ints.
    Additionally this commit also adds a new query for benchmarking and
    cleans up/documents some of the existing code.

--- cmd/bulk_query_gen/iobeam_devops_common.go
+++ cmd/bulk_query_gen/iobeam_devops_common.go
@@ -54,30 +54,37 @@ func (d *IobeamDevops) Dispatch(i, scaleVar int) Query {
 	return q
 }
 
+// MaxCPUUsageHourByMinuteOneHost runs benchmark for max(cpu) per minute over 1 hour on 1 host
 func (d *IobeamDevops) MaxCPUUsageHourByMinuteOneHost(q Query, scaleVar int) {
 	d.maxCPUUsageHourByMinuteNHosts(q, scaleVar, 1, time.Hour)
 }
 
+// MaxCPUUsageHourByMinuteTwoHosts runs benchmark for max(cpu) per minute over 1 hour on 2 hosts
 func (d *IobeamDevops) MaxCPUUsageHourByMinuteTwoHosts(q Query, scaleVar int) {
 	d.maxCPUUsageHourByMinuteNHosts(q, scaleVar, 2, time.Hour)
 }
 
+// MaxCPUUsageHourByMinuteFourHosts runs benchmark for max(cpu) per minute over 1 hour on 4 hosts
 func (d *IobeamDevops) MaxCPUUsageHourByMinuteFourHosts(q Query, scaleVar int) {
 	d.maxCPUUsageHourByMinuteNHosts(q, scaleVar, 4, time.Hour)
 }
 
+// MaxCPUUsageHourByMinuteEightHosts runs benchmark for max(cpu) per minute over 1 hour on 8 hosts
 func (d *IobeamDevops) MaxCPUUsageHourByMinuteEightHosts(q Query, scaleVar int) {
 	d.maxCPUUsageHourByMinuteNHosts(q, scaleVar, 8, time.Hour)
 }
 
+// MaxCPUUsageHourByMinuteSixteenHosts runs benchmark for max(cpu) per minute over 1 hour on 16 hosts
 func (d *IobeamDevops) MaxCPUUsageHourByMinuteSixteenHosts(q Query, scaleVar int) {
 	d.maxCPUUsageHourByMinuteNHosts(q, scaleVar, 16, time.Hour)
 }
 
+// MaxCPUUsageHourByMinuteThirtyTwoHosts runs benchmark for max(cpu) per minute over 1 hour on 32 hosts
 func (d *IobeamDevops) MaxCPUUsageHourByMinuteThirtyTwoHosts(q Query, scaleVar int) {
 	d.maxCPUUsageHourByMinuteNHosts(q, scaleVar, 32, time.Hour)
 }
 
+// MaxCPUUsage12HoursByMinuteOneHost runs benchmark for max(cpu) per minute over 12 hours on 1 host
 func (d *IobeamDevops) MaxCPUUsage12HoursByMinuteOneHost(q Query, scaleVar int) {
 	d.maxCPUUsageHourByMinuteNHosts(q, scaleVar, 1, 12*time.Hour)
 }
@@ -90,14 +97,16 @@ func (d *IobeamDevops) MaxAllCPUHourByMinuteEightHosts(q Query, scaleVar int) {
 	d.maxAllCPUHourByMinuteNHosts(q, scaleVar, 8)
 }
 
+const goTimeFmt = "2006-01-02 15:04:05.999999 -7:00"
+
 // MaxCPUUsageHourByMinuteThirtyTwoHosts populates a Query with a query that looks like:
 // SELECT max(usage_user) from cpu where (hostname = '$HOSTNAME_1' or ... or hostname = '$HOSTNAME_N') and time >= '$HOUR_START' and time < '$HOUR_END' group by time(1m)
 func (d *IobeamDevops) maxCPUUsageHourByMinuteNHosts(qi Query, scaleVar, nhosts int, timeRange time.Duration) {
 	interval := d.AllInterval.RandWindow(timeRange)
 
-	sqlQuery := fmt.Sprintf(`SELECT date_trunc('minute'::text, io2ts(time)) AS minute, max(usage_user) FROM cpu where %s AND time >= %d AND time < %d GROUP BY minute ORDER BY minute ASC`, d.getHostWhereString(scaleVar, nhosts), interval.Start.UnixNano(), interval.End.UnixNano())
+	sqlQuery := fmt.Sprintf(`SELECT date_trunc('minute', time) AS minute, max(usage_user) FROM cpu where %s AND time >= '%s' AND time < '%s' GROUP BY minute ORDER BY minute ASC`, d.getHostWhereString(scaleVar, nhosts), interval.Start.Format(goTimeFmt), interval.End.Format(goTimeFmt))
 
-	humanLabel := fmt.Sprintf("Iobeam max cpu, rand %4d hosts, rand %s by 1m", nhosts, timeRange)
+	humanLabel := fmt.Sprintf("TimescaleDB max cpu, rand %4d hosts, rand %s by 1m", nhosts, timeRange)
 	q := qi.(*IobeamQuery)
 	q.HumanLabel = []byte(humanLabel)
 	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, interval.StartString()))
@@ -106,9 +115,11 @@ func (d *IobeamDevops) maxCPUUsageHourByMinuteNHosts(qi Query, scaleVar, nhosts 
 	q.SqlQuery = []byte(sqlQuery)
 }
 
+// GroupByOrderByLimit benchmarks a query that has a time WHERE clause, that groups by a truncated date, orders by that date, and takes a limit:
+// SELECT date_trunc('minute', time) as t, MAX(cpu) FROM cpu WHERE time < '$TIME' GROUP BY t ORDER BY t DESC LIMIT $LIMIT
 func (d *IobeamDevops) GroupByOrderByLimit(qi Query, _ int) {
 	interval := d.AllInterval.RandWindow(12 * time.Hour)
-	timeStr := interval.End.Format("2006-01-02 15:04:05.999999 -7:00")
+	timeStr := interval.End.Format(goTimeFmt)
 
 	where := fmt.Sprintf("WHERE time < '%s'", timeStr)
 
@@ -128,9 +139,9 @@ func (d *IobeamDevops) GroupByOrderByLimit(qi Query, _ int) {
 func (d *IobeamDevops) MeanCPUUsageDayByHourAllHostsGroupbyHost(qi Query, _ int) {
 	interval := d.AllInterval.RandWindow(24 * time.Hour)
 
-	sqlQuery := fmt.Sprintf(`SELECT date_trunc('hour'::text, io2ts(time)) as hour, hostname, avg(usage_user) as mean_usage_user FROM cpu WHERE time >= %d AND time < %d GROUP BY hour, hostname ORDER BY hour`, interval.Start.UnixNano(), interval.End.UnixNano())
+	sqlQuery := fmt.Sprintf(`SELECT date_trunc('hour', time) as hour, hostname, avg(usage_user) as mean_usage_user FROM cpu WHERE time >= '%s' AND time < '%s' GROUP BY hour, hostname ORDER BY hour`, interval.Start.Format(goTimeFmt), interval.End.Format(goTimeFmt))
 
-	humanLabel := "Iobeam mean cpu, all hosts, rand 1day by 1hour"
+	humanLabel := "TimescaleDB mean cpu, all hosts, rand 1day by 1hour"
 	q := qi.(*IobeamQuery)
 	q.HumanLabel = []byte(humanLabel)
 	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, interval.StartString()))
@@ -145,9 +156,9 @@ func (d *IobeamDevops) MeanCPUUsageDayByHourAllHostsGroupbyHost(qi Query, _ int)
 func (d *IobeamDevops) maxAllCPUHourByMinuteNHosts(qi Query, scaleVar, nhosts int) {
 	interval := d.AllInterval.RandWindow(12 * time.Hour)
 
-	sqlQuery := fmt.Sprintf(`SELECT date_trunc('hour'::text, io2ts(time)) AS hour, max(usage_user) as max_usage_user FROM cpu where %s AND time >= %d AND time < %d GROUP BY hour ORDER BY hour`, d.getHostWhereString(scaleVar, nhosts), interval.Start.UnixNano(), interval.End.UnixNano())
+	sqlQuery := fmt.Sprintf(`SELECT date_trunc('hour', time) AS hour, max(usage_user) as max_usage_user FROM cpu where %s AND time >= '%s' AND time < '%s' GROUP BY hour ORDER BY hour`, d.getHostWhereString(scaleVar, nhosts), interval.Start.Format(goTimeFmt), interval.End.Format(goTimeFmt))
 
-	humanLabel := fmt.Sprintf("Iobeam max cpu all fields, rand %4d hosts, rand 12hr by 1m", nhosts)
+	humanLabel := fmt.Sprintf("TimescaleDB max cpu all fields, rand %4d hosts, rand 12hr by 1m", nhosts)
 	q := qi.(*IobeamQuery)
 	q.HumanLabel = []byte(humanLabel)
 	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, interval.StartString()))
@@ -161,7 +172,7 @@ func (d *IobeamDevops) LastPointPerHost(qi Query, _ int) {
 
 	sqlQuery := fmt.Sprintf(`SELECT DISTINCT ON (hostname) * FROM cpu ORDER BY hostname, time DESC`)
 
-	humanLabel := "Iobeam last row per host"
+	humanLabel := "TimescaleDB last row per host"
 	q := qi.(*IobeamQuery)
 	q.HumanLabel = []byte(humanLabel)
 	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, measure))
@@ -177,7 +188,7 @@ func (d *IobeamDevops) LastPointPerHost(qi Query, _ int) {
 //	v.Set("db", d.DatabaseName)
 //	v.Set("q", fmt.Sprintf("SELECT count(usage_user) from cpu where time >= '%s' and time < '%s' group by time(1h)", interval.StartString(), interval.EndString()))
 //
-//	humanLabel := "Iobeam mean cpu, all hosts, rand 1day by 1hour"
+//	humanLabel := "TimescaleDB mean cpu, all hosts, rand 1day by 1hour"
 //	q := qi.(*HTTPQuery)
 //	q.HumanLabel = []byte(humanLabel)
 //	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, interval.StartString()))
@@ -191,9 +202,9 @@ func (d *IobeamDevops) LastPointPerHost(qi Query, _ int) {
 func (d *IobeamDevops) HighCPU(qi Query, _ int) {
 	interval := d.AllInterval.RandWindow(24 * time.Hour)
 
-	sqlQuery := fmt.Sprintf(`SELECT * FROM cpu WHERE usage_user > 90.0 AND time >= %d AND time < %d`, interval.Start.UnixNano(), interval.End.UnixNano())
+	sqlQuery := fmt.Sprintf(`SELECT * FROM cpu WHERE usage_user > 90.0 AND time >= '%s' AND time < '%s'`, interval.Start.Format(goTimeFmt), interval.End.Format(goTimeFmt))
 
-	humanLabel := "Iobeam cpu over threshold, all hosts"
+	humanLabel := "TimescaleDB cpu over threshold, all hosts"
 	q := qi.(*IobeamQuery)
 	q.HumanLabel = []byte(humanLabel)
 	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, interval.StartString()))
@@ -207,9 +218,9 @@ func (d *IobeamDevops) HighCPUAndField(qi Query, hosts int) {
 	interval := d.AllInterval.RandWindow(24 * time.Hour)
 	hostName := fmt.Sprintf("host_%d", rand.Intn(hosts))
 
-	sqlQuery := fmt.Sprintf(`SELECT * FROM cpu WHERE usage_user > 90.0 and time >= %d AND time < %d and hostname = '%s'`, interval.Start.UnixNano(), interval.End.UnixNano(), hostName)
+	sqlQuery := fmt.Sprintf(`SELECT * FROM cpu WHERE usage_user > 90.0 and time >= '%s' AND time < '%s' and hostname = '%s'`, interval.Start.Format(goTimeFmt), interval.End.Format(goTimeFmt), hostName)
 
-	humanLabel := "Iobeam cpu over threshold, all hosts"
+	humanLabel := "TimescaleDB cpu over threshold, all hosts"
 	q := qi.(*IobeamQuery)
 	q.HumanLabel = []byte(humanLabel)
 	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, interval.StartString()))
@@ -223,9 +234,9 @@ func (d *IobeamDevops) HighCPUAndField(qi Query, hosts int) {
 func (d *IobeamDevops) MultipleMemOrs(qi Query, hosts int) {
 	interval := d.AllInterval.RandWindow(24 * time.Hour)
 
-	sqlQuery := fmt.Sprintf(`SELECT * FROM mem WHERE used_percent > 98.0 OR used > 10000 OR used_percent < 5.0 AND time >= %d AND time < %d`, interval.Start.UnixNano(), interval.End.UnixNano())
+	sqlQuery := fmt.Sprintf(`SELECT * FROM mem WHERE used_percent > 98.0 OR used > 10000 OR used_percent < 5.0 AND time >= '%s' AND time < '%s'`, interval.Start.Format(goTimeFmt), interval.End.Format(goTimeFmt))
 
-	humanLabel := "Iobeam mem fields with or, all hosts"
+	humanLabel := "TimescaleDB mem fields with or, all hosts"
 	q := qi.(*IobeamQuery)
 	q.HumanLabel = []byte(humanLabel)
 	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, interval.StartString()))
@@ -237,9 +248,9 @@ func (d *IobeamDevops) MultipleMemOrs(qi Query, hosts int) {
 func (d *IobeamDevops) MultipleMemOrsByHost(qi Query, hosts int) {
 	interval := d.AllInterval.RandWindow(24 * time.Hour)
 
-	sqlQuery := fmt.Sprintf(`SELECT date_trunc('hour'::text, io2ts(time)) AS hour, MAX(used_percent) from mem where used < 1000 or used_percent > 98.0 or used_percent < 10.0 and time >= %d and time < %d GROUP BY hour,hostname`, interval.Start.UnixNano(), interval.End.UnixNano())
+	sqlQuery := fmt.Sprintf(`SELECT date_trunc('hour', time) AS hour, MAX(used_percent) from mem where used < 1000 or used_percent > 98.0 or used_percent < 10.0 and time >= '%s' and time < '%s' GROUP BY hour,hostname`, interval.Start.Format(goTimeFmt), interval.End.Format(goTimeFmt))
 
-	humanLabel := "Iobeam mem fields with or, all hosts"
+	humanLabel := "TimescaleDB mem fields with or, all hosts"
 	q := qi.(*IobeamQuery)
 	q.HumanLabel = []byte(humanLabel)
 	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel, interval.StartString()))
