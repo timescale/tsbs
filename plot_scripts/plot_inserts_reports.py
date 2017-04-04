c4aa4768f20809f82a1876a9b8bc9345c7beafdc
Michael J. Freedman <mfreed@cs.princeton.edu>
2017-04-03 21:34:19 -0400

    Fancier plotting for slides, updated query times

--- plot_scripts/plot_inserts_reports.py
+++ plot_scripts/plot_inserts_reports.py
@@ -9,6 +9,8 @@ import sys
 logline_with_time_re =re.compile(r"REPORT: time (\d+) col rate (?P<col_rate>\d+\.\d+)/sec row rate (?P<row_rate>\d+\.\d+)/sec " \
                        r"\(period\) (?P<period>\d+\.\d+)/sec \(total\) total rows (?P<total_rows>\d+\.\d+[E0-9+]+)")
 
+report_period = 20000
+
 def read_log_file(filename, regexp=logline_with_time_re):
 
     with open(filename, 'rb') as datafile:
@@ -26,12 +28,13 @@ def plot_series(x, y, plot_trend, style):
     str_style = "%s%s%s" % (style['line'], style['marker'], style['color'])
 
     if plot_trend:
-        plt.scatter(x, y, marker=style['marker'], color=style['color'], alpha=0.4)
+        plt.scatter(x, y, marker=style['marker'], color=style['color'], alpha=style['alpha'])
         coefs = poly.polyfit(x, y, 3)
         ffit = poly.polyval(x, coefs)
         plt.plot(x, ffit, str_style.replace('*', '').replace('+', ''), lw=style['lw'], label=style['label'])
     else:
-        plt.plot(x, y, str_style, label=style['label'], lw=style['lw'])
+        #plt.plot(x, y, str_style, label=style['label'], lw=style['lw'])
+        plt.scatter(x, y, marker=style['marker'], color=style['color'], alpha=style['alpha'], label=style['label'])
 
 
 def smooth_it(x,y, downsample=100):
@@ -44,6 +47,68 @@ def smooth_it(x,y, downsample=100):
     return smoothed_x * downsample, smoothed_y
 
 
+def plot_plain(args):
+    plain_dir = args.dir + '/plain'
+
+    if True:
+        plain = read_log_file('{}/load_plain_{}_{}.log'.format(plain_dir, args.batch_size, report_period))
+        xplain, yplain = plain[:, 4], plain[:, 2]
+        alpha = 0.5 if args.trend else 0.8
+        style = { 'line': '-', 'marker': '+', 'color': 'b', 'label': "PostgreSQL", 'lw': 3, 'alpha': alpha }
+        plot_series(xplain, yplain, args.trend, style)
+        plain_time = (plain[-1,0] - plain[0,0]) / 3600
+    else:
+        plain_time = 0
+
+    return plain_time
+
+
+def plot_both(args):
+
+    hyper_dir = args.dir + '/hypertable'
+
+    plain_time = plot_plain(args)
+
+    alpha = 0.3 if args.batch_size < 1000 else 0.6 if args.trend else 9
+
+    hyper = read_log_file('{}/load_hypertable_{}_{}.log'.format(hyper_dir, args.batch_size, report_period))
+    xhyper, yhyper = hyper[:, 4], hyper[:, 2]
+    style = { 'line': '-', 'marker': '*', 'color': 'r', 'label': "TimescaleDB", 'lw': 3, 'alpha': alpha}
+    plot_series(xhyper, yhyper, args.trend, style)
+    hyper_time = (hyper[-1, 0] - hyper[0, 0]) / 3600
+
+    if not args.pretty:
+        plt.title("Insert batch size: {}, Cache: {} GB memory, plain {:.1f} h, hyper table {:.1f} h".format(args.batch_size, args.memory, plain_time, hyper_time))
+
+
+def generate_figure(args, desc):
+
+    plt.xlabel("Dataset size [millions of rows]")
+    plt.ylabel("Insert rate [rows / second]")
+    plt.ticklabel_format(style='plain', axis='y') #, scilimits=(0, 0))
+
+    axes = plt.gca()
+    axes.get_xaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/1e6)))
+    axes.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    axes.set_xlim(left=0, right=270000000)
+    axes.set_ylim(bottom=0)
+
+    if args.pretty:
+        plt.title("Insert batch size: {},  Cache: {} GB memory".format(args.batch_size, args.memory))
+
+    # 16:9
+    scale=1.8
+    plt.gcf().set_size_inches((16./scale), (9/scale))
+
+    if args.trend:
+        plt.legend(frameon=False)
+    else:
+        plt.legend(frameon=False, numpoints=3)
+
+    #plt.savefig('write_performance_{}_{}_{}-{}.png'.format(args.batch_size, report_period, args.memory, desc), dpi=600, transparent=True)
+    plt.savefig('write_performance_{}_{}_{}-{}.pdf'.format(args.batch_size, report_period, args.memory, desc), format='pdf')
+    # plt.show()
+
 
 if __name__ == '__main__':
 
@@ -51,54 +116,23 @@ if __name__ == '__main__':
     parser.add_argument('-b', dest='batch_size', default=10000, type=int)
     parser.add_argument('-d', dest='dir', default='new_insert_no_debug', type=str)
     parser.add_argument('-p', dest='pretty', default=False, action='store_true')
+    parser.add_argument('-m', dest='memory', default=16, type=int)
     parser.add_argument('--trend', dest='trend', default=False, action='store_true')
     args = parser.parse_args()
 
-    report_period = 20000
+
     # workers = 10
     # data_set_length = '5d'
-
-    plain_dir = args.dir + '/plain'
-    hyper_dir = args.dir + '/hypertable'
 
     if args.batch_size <= 0:
         print 'Batch size must be > 0'
         sys.exit(1);
 
-    if True:
-        plain = read_log_file('{}/load_plain_{}_{}.log'.format(plain_dir, args.batch_size, report_period))
-        xplain, yplain = plain[:, 4], plain[:, 2]
-        style = { 'line': '-', 'marker': '*', 'color': 'b', 'label': "plain", 'lw': 2 }
-        plot_series(xplain, yplain, args.trend, style)
-        plain_time = (plain[-1,0] - plain[0,0]) / 3600
-    else:
-        plain_time = 0
-
-    hyper = read_log_file('{}/load_hypertable_{}_{}.log'.format(hyper_dir, args.batch_size, report_period))
-    xhyper, yhyper = hyper[:, 4], hyper[:, 2]
-    style = { 'line': '-', 'marker': '+', 'color': 'r', 'label': "hypertable", 'lw': 2 }
-    plot_series(xhyper, yhyper, args.trend, style)
-    hyper_time = (hyper[-1, 0] - hyper[0, 0]) / 3600
+    plot_both(args)
+    generate_figure(args, "both")
+    plt.clf()
+    plot_plain(args)
+    generate_figure(args, "plain")
 
 
-    if args.pretty:
-        plt.title("Insert batch size: {}".format(args.batch_size))
-    else:
-        plt.title("Insert batch size: {}, plain {:.1f} h, hyper table {:.1f} h".format(args.batch_size, plain_time, hyper_time))
 
-    plt.xlabel("Database size [millions of rows]")
-    plt.ylabel("Insert rate [rows / second]")
-    plt.ticklabel_format(style='sci', axis='y') #, scilimits=(0, 0))
-
-    axes = plt.gca()
-    axes.get_xaxis().set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/1e6)))
-    axes.set_xlim(left=0)
-    axes.set_ylim(bottom=0)
-
-    # 16:9
-    scale=1.8
-    plt.gcf().set_size_inches((16./scale), (9/scale))
-
-    plt.legend(frameon=False)
-    plt.savefig('write_performance_{}_{}.png'.format(args.batch_size,report_period))
-    # plt.show()
--- plot_scripts/plot_queries_bars.py
+++ plot_scripts/plot_queries_bars.py
@@ -1,15 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 # query, % diff, plain min [ms], plain mean, plain max, ht min, ht mean, ht max
-query_times = [('cpu-max-all-eight-hosts',5.95,24636.7,35724.14,56855.98,23180.64,33599.56,47687.26),\
-               ('cpu-max-all-single-host',19.51,27.61,717.32,1788.11,31.62,577.39,1311.18),\
-               ('groupby',68.92,47564.97,92368.51,158756.43,8322.97,28709.68,64471.76),\
-               ('high-cpu-and-field',2.22,45.02,1050.97,2582.23,55.07,1027.61,2562.32),\
-               ('high-cpu',1.60,12850.14,27368.16,39802.75,11289.32,26929.22,39665.32),\
-               ('lastpoint (Only 5 runs)',1.31,348764.34,348982.27,349300.67,337359.23,344413.99,347810.75),\
-               ('single-host',10.10,17.48,973.66,8085.21,23.58,875.35,5053.82)]
+#('cpu-max-all-eight-hosts',
+#('cpu-max-all-single-host'
+#('groupby'
+#('high-cpu-and-field'
+#('high-cpu'
+#('lastpoint (Only 5 runs)'
+#('single-host'
+
+query_times = [('high vals for 1 device\n over time interval',2.27,45.02,1050.97,2582.23,55.07,1027.61,2562.32),\
+#               ('high vals for all devices',1.60,12850.14,27368.16,39802.75,11289.32,26929.22,39665.32),\
+#               ('latest per device',1.31,348764.34,348982.27,349300.67,337359.23,344413.99,347810.75),\
+               ('max per min for 1 device\n over time interval',11.23,17.48,973.66,8085.21,23.58,875.35,5053.82),\
+               ('max per hour for 1 device\n over time interval',24.23,27.61,717.32,1788.11,31.62,577.39,1311.18),\
+#               ('max per hour for 8 devices',6.32,24636.7,35724.14,56855.98,23180.64,33599.56,47687.26),\
+               ('avg per hour per every device \n over time interval',221.73,47564.97,92368.51,158756.43,8322.97,28709.68,64471.76),\
+               ('max per min across all devices \n with limit',454.68,4658.80, 6367.10, 8556.71,23.85, 1147.88,2952.15)]
 
 def make_query_barplot(query_data):
     plt.rcdefaults()
@@ -22,15 +32,30 @@ def make_query_barplot(query_data):
     percentage_diff = [q[1] for q in query_data]
 
     ax.barh(y_pos, percentage_diff, align='center',
-            color='green', ecolor='black')
+            color='blue', ecolor='black', height=0.6)
     ax.set_yticks(y_pos)
     ax.set_yticklabels(query_names)
     ax.invert_yaxis()  # labels read top-to-bottom
-    ax.set_xlabel('Query latency difference [%]')
+    ax.set_xlabel('Query latency improvement [%]')
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(50))
+    plt.xlim([-20,510])
 
-    plt.xlim([-20,100])
-    plt.subplots_adjust(left=0.25, right=0.9, top=0.9, bottom=0.1)
-    plt.show()
+    rects = ax.patches
+
+    # Now make some labels
+    labels = ["%d" % q[1] for q in query_data]
+
+    for rect, label in zip(rects, labels):
+        ax.text(rect.get_width() + 8, rect.get_y()+.3, label+"%", ha='left', va='center')
+
+    # 16:9
+    scale=1.3
+    plt.gcf().set_size_inches((16./scale), (9/scale))
+
+    plt.subplots_adjust(left=0.3, right=.9, top=0.6, bottom=0.1)
+    plt.savefig('query_performance.pdf', format='pdf')
+    #plt.savefig('query_performance.png', dpi=300, transparent=True)
+    #plt.show()
 
 if __name__ == '__main__':
     make_query_barplot(query_times)
