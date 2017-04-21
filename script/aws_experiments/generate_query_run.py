42bee619bbfe42cd412c2c55434e7b587c6ff6fd
Rob Kiefer <rob.kiefer@gmail.com>
2017-04-21 12:56:50 -0400

    Generalize the generate_queries script

--- script/aws_experiments/generate_query_run.py
+++ script/aws_experiments/generate_query_run.py
@@ -1,37 +1,46 @@
 import argparse
+import os
 
-def get_load_str(load_file, batch_size, workers, reporting_period=20000):
-    logfilename = 'load_influxdb_{}_{}.log'.format(batch_size, reporting_period)
+def get_load_str(load_file, label, batch_size, workers, reporting_period=20000):
+    logfilename = 'load_{}_{}_{}.log'.format(label, batch_size, reporting_period)
 
-    return 'NUM_WORKERS={} DATA_DIR=/tmp BULK_DATA_DIR=/mnt/devops DATABASE_HOST=localhost BATCH_SIZE={} ./load_influxdb.sh | tee {}'.format(
-    workers, batch_size, logfilename)
+    if label == "influxdb":
+        return 'NUM_WORKERS={} DATA_DIR=/tmp BULK_DATA_DIR=/mnt/devops DATABASE_HOST=localhost BATCH_SIZE={} ./load_{}.sh | tee {}'.format(
+            workers, batch_size, label, logfilename)
+    elif label == "cassandra":
+        return 'NUM_WORKERS={} DATA_DIR=/tmp BULK_DATA_DIR=/mnt/devops DATABASE_HOST=localhost CASSANDRA_BATCH_SIZE={} ./load_{}.sh | tee {}'.format(
+            workers, batch_size, label, logfilename)
+
 
 def get_query_str(queryfile, label, workers=10, limit=None):
-    return 'cat {} | gunzip | query_benchmarker_influxdb -workers {} {} | tee {}'\
-        .format(queryfile, workers, '-limit {}'.format(limit) if limit is not None else '',
+    return 'cat {} | gunzip | query_benchmarker_{} -workers {} {} | tee {}'\
+        .format(queryfile, label, workers,
+                '-limit {}'.format(limit) if limit is not None else '',
                 'query_{}_{}'.format(label, queryfile.split('/')[-1]).split('.')[0])
 
-def load_queries_file_names(filename):
+def load_queries_file_names(filename, label, query_dir='/mnt/devops'):
     l = list()
     with open(filename, 'r') as queries:
         for query in queries:
             query = query.split('#')[0]
             if len(query) > 0:
-                l.append(query.strip())
+                l.append(os.path.join(query_dir, "{}-{}-queries.gz".format(label, query.strip())))
 
     return l
 
-def generate_run_file(queries_file, load_file, batch_sizes, workers=8):
+def generate_run_file(queries_file, load_file, db_name, batch_sizes, workers=8):
 
     print '#!/bin/bash'
     queries = None
-    if queries_file != None:
-        queries = load_queries_file_names(queries_file)
+    if queries_file is not None:
+        queries = load_queries_file_names(queries_file, db_name)
 
-    for batch_size in batch_sizes:
-        print get_load_str(load_file, batch_size, workers)
-        for query in queries:
-            print get_query_str(query, 'influxdb', workers=workers)
+    if load_file is not None:
+        for batch_size in batch_sizes:
+            print get_load_str(load_file, db_name, batch_size, workers)
+
+    for query in queries:
+        print get_query_str(query, db_name, workers=workers)
 
 
 if __name__ == "__main__":
@@ -42,12 +51,19 @@ if __name__ == "__main__":
     parser.add_argument('-f', dest='queries_file_name', default='queries.txt', type=str)
     parser.add_argument('-l', dest='load_file_name', default=default_load_file, type=str)
     parser.add_argument('-w', dest='write_only', default=False, action='store_true')
+    parser.add_argument('-d', dest='db_name', default=None, type=str)
+    parser.add_argument('-q', dest='query_only', default=False, action='store_true')
 
     args = parser.parse_args()
+
+    if args.db_name is None:
+        print("Usage: generate_query_run.py -d db_name")
+        exit(1)
 
     batch_sizes = [int(b) for b in args.batch_sizes_str.split(',')]
 
     generate_run_file(
-	args.queries_file_name if not args.write_only else None,
-	args.load_file_name,
-	batch_sizes)
+        args.queries_file_name if not args.write_only else None,
+        args.load_file_name if not args.query_only else None,
+        args.db_name,
+        batch_sizes)
