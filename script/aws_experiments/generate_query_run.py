316dcb6b132e2682972dd24e3fa52f5e2766c47b
Rob Kiefer <rob.kiefer@gmail.com>
2017-05-03 16:24:44 -0400

    Cleanup benchmarking scripts and update them.
    Remove unused query_*.sh files and add support for timescaledb with other
    scripts for cassandra and influx. Add usage instructions to scripts.

--- script/aws_experiments/generate_query_run.py
+++ script/aws_experiments/generate_query_run.py
@@ -1,21 +1,71 @@
+'''Generates bash scripts to run benchmarks for a variety of databases.
+
+This script generates a bash script that can be run as part of a benchmarking
+pipline. The output of the script relies on load_* scripts in the same directory
+for inserting, and on Go binaries generated from Influx's benchmark suite.
+
+Usage flags:
+    -b      Sets the batch size for inserting (default: 10000)
+
+    -d      Database to benchmark. Valid values: cassandra, influxdb, timescaledb
+
+    -f      Filename containing query benchmark names, one per line. Lines can
+            be commented out to exclude those benchmarks (default: queries.txt)
+
+    -i      Insert benchmark only, no queries (default: false)
+
+    -l      Directory where input files are located. They should be named
+            `[database]-data.gz` (default: /tmp)
+
+    -o      Directory where query files are located (default: /tmp/queries)
+
+    -q      Query benchmarks only, no insert. Data needs to be previously
+            inserted (default: false)
+
+    -w      Number of workers/threads to use (default: 4)
+
+EXAMPLE:
+
+queries.txt:
+#1-host-12-hr
+5-metrics-1-host-1-hr
+5-metrics-1-host-12-hr
+
+Command:
+python generate_query_run -d timescaledb -w 8
+
+Result:
+#!/bin/bash
+# Load data
+NUM_WORKERS=8 DATA_DIR=/tmp BULK_DATA_DIR=/tmp DATABASE_HOST=localhost BATCH_SIZE=10000 ./load_timescaledb.sh | tee load_timescaledb_10000_20000.out
+
+# Queries
+cat /tmp/queries/timescaledb-5-metrics-1-host-1-hr-queries.gz | gunzip | query_benchmarker_timescaledb -workers 8 -limit 1000 -postgres "host=localhost user=postgres sslmode=disable database=benchmark timescaledb.disable_optimizations=false" | tee query_timescaledb_timescaledb-5-metrics-1-host-1-hr-queries.out
+
+cat /tmp/queries/timescaledb-5-metrics-1-host-12-hr-queries.gz | gunzip | query_benchmarker_timescaledb -workers 8 -limit 1000 -postgres "host=localhost user=postgres sslmode=disable database=benchmark timescaledb.disable_optimizations=false" | tee query_timescaledb_timescaledb-5-metrics-1-host-12-hr-queries.out
+'''
 import argparse
 import os
 
-def get_load_str(load_file, label, batch_size, workers, reporting_period=20000):
+def get_load_str(load_dir, label, batch_size, workers, reporting_period=20000):
     logfilename = 'load_{}_{}_{}.out'.format(label, batch_size, reporting_period)
+    prefix = 'NUM_WORKERS={} DATA_DIR=/tmp BULK_DATA_DIR={} DATABASE_HOST=localhost'.format(workers, load_dir)
+    suffix = ' ./load_{}.sh | tee {}'.format(label, logfilename)
 
     if label == 'influxdb':
-        return 'NUM_WORKERS={} DATA_DIR=/tmp BULK_DATA_DIR=/mnt/devops DATABASE_HOST=localhost BATCH_SIZE={} ./load_{}.sh | tee {}'.format(
-            workers, batch_size, label, logfilename)
+        return prefix + ' BATCH_SIZE={}'.format(batch_size) + suffix
     elif label == 'cassandra':
-        return 'NUM_WORKERS={} DATA_DIR=/tmp BULK_DATA_DIR=/mnt/devops DATABASE_HOST=localhost CASSANDRA_BATCH_SIZE={} ./load_{}.sh | tee {}'.format(
-            workers, batch_size, label, logfilename)
+        return prefix + ' CASSANDRA_BATCH_SIZE={}'.format(batch_size) + suffix
+    elif label == 'timescaledb':
+        return prefix + ' BATCH_SIZE={}'.format(batch_size) + suffix
 
 
-def get_query_str(queryfile, label, workers=10, limit=None):
+def get_query_str(queryfile, label, workers, limit=1000):
     extra_args = ''
     if label == 'cassandra':
-        extra_args = '--aggregation-plan client'
+        extra_args = '-aggregation-plan client'
+    elif label == 'timescaledb':
+        extra_args = '-postgres "{}"'.format('host=localhost user=postgres sslmode=disable database=benchmark timescaledb.disable_optimizations=false')
 
     limit_arg = '-limit {}'.format(limit) if limit is not None else ''
     output_file = 'query_{}_{}'.format(label, queryfile.split('/')[-1]).split('.')[0]
@@ -23,7 +73,7 @@ def get_query_str(queryfile, label, workers=10, limit=None):
     return 'cat {} | gunzip | query_benchmarker_{} -workers {} {} {} | tee {}.out'.format(
         queryfile, label, workers, limit_arg, extra_args, output_file)
 
-def load_queries_file_names(filename, label, query_dir='/mnt/queries'):
+def load_queries_file_names(filename, label, query_dir):
     l = list()
     with open(filename, 'r') as queries:
         for query in queries:
@@ -34,31 +84,39 @@ def load_queries_file_names(filename, label, query_dir='/mnt/queries'):
 
     return l
 
-def generate_run_file(queries_file, load_file, db_name, batch_sizes, workers=8):
+def generate_run_file(queries_file, query_dir, load_dir, db_name, batch_sizes, workers):
 
     print '#!/bin/bash'
     queries = None
     if queries_file is not None:
-        queries = load_queries_file_names(queries_file, db_name)
+        queries = load_queries_file_names(queries_file, db_name, query_dir)
 
-    if load_file is not None:
+    if load_dir is not None:
         for batch_size in batch_sizes:
-            print get_load_str(load_file, db_name, batch_size, workers)
+            print("# Load data")
+            print(get_load_str(load_dir, db_name, batch_size, workers))
+            print("")
 
+    if queries is not None:
+        print("# Queries")
     for query in queries:
-        print get_query_str(query, db_name, workers=workers)
+        print(get_query_str(query, db_name, workers))
+        print("")
 
 
 if __name__ == "__main__":
-    default_load_file = '/mnt/devops/influx-bulk-data.gz'
+    default_load_dir = '/tmp'
+    default_query_dir = '/tmp/queries'
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', dest='batch_sizes_str', default="10000", type=str)
-    parser.add_argument('-f', dest='queries_file_name', default='queries.txt', type=str)
-    parser.add_argument('-l', dest='load_file_name', default=default_load_file, type=str)
-    parser.add_argument('-w', dest='write_only', default=False, action='store_true')
     parser.add_argument('-d', dest='db_name', default=None, type=str)
+    parser.add_argument('-f', dest='queries_file_name', default='queries.txt', type=str)
+    parser.add_argument('-i', dest='write_only', default=False, action='store_true')
+    parser.add_argument('-l', dest='load_file_dir', default=default_load_dir, type=str)
+    parser.add_argument('-o', dest='query_file_dir', default=default_query_dir, type=str)
     parser.add_argument('-q', dest='query_only', default=False, action='store_true')
+    parser.add_argument('-w', dest='workers', default=4, type=int)
 
     args = parser.parse_args()
 
@@ -70,6 +128,8 @@ if __name__ == "__main__":
 
     generate_run_file(
         args.queries_file_name if not args.write_only else None,
-        args.load_file_name if not args.query_only else None,
+        args.query_file_dir if not args.write_only else None,
+        args.load_file_dir if not args.query_only else None,
         args.db_name,
-        batch_sizes)
+        batch_sizes,
+        args.workers)
---
+++ script/aws_experiments/load_timescaledb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+EXE_DIR=${EXE_DIR:-$(dirname $0)}
+DATA_FILE_NAME=${DATA_FILE_NAME:-timescaledb-data.gz}
+PROGRESS_INTERVAL=${PROGRESS_INTERVAL:-20000}
+
+source ${EXE_DIR}/load_common.sh
+source ${EXE_DIR}/iobeamdb.conf
+
+while ! pg_isready; do
+    echo "Waiting for timescaledb"
+    sleep 1
+done
+
+cat ${DATA_FILE} | gunzip | bulk_load_timescaledb \
+                                --batch-size ${BATCH_SIZE} \
+                                --field-index="VALUE-TIME" \
+                                --field-index-count=1 \
+                                --make-hypertable=true \
+                                --number_partitions 1 \
+                                --workers ${NUM_WORKERS}\
+                                --postgres "host=${DATABASE_HOST} user=postgres sslmode=disable" \
+                                --reporting-period ${PROGRESS_INTERVAL} \
+                                --tag-index="VALUE-TIME" \
+                                --batch-size=${BATCH_SIZE}
