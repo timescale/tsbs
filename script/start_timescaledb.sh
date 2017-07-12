ae4fda10ce7d2be4eb3b3bbd907bfd03e3e9c2f9
Rob Kiefer <rob@timescale.com>
2017-07-12 14:46:20 -0400

    Add start & stop scripts for TimescaleDB (for Ubuntu)
    The scripts will start TimescaleDB with all available memory and
    then divide it up to various caches using settings suggested by
    PgTune and other online docs.

---
+++ script/start_timescaledb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+MEM=${MEM:-`free -m | grep "Mem" | awk '{print $7}'`}
+let "SHARED=$MEM/4"
+let "CACHE=2*$MEM/3"
+let "WORK=($MEM-$SHARED)/30"
+let "MAINT=$MEM/16"
+
+sudo -u postgres /usr/lib/postgresql/9.6/bin/pg_ctl -c -U postgres -D /etc/postgresql/9.6/main -l /tmp/postgres.log -o "-cshared_preload_libraries=timescaledb \
+ -clog_line_prefix=\"%m [%p]: [%x] %u@%d\" -clogging_collector=off \
+ -csynchronous_commit=off \
+ -cmax_wal_size=10GB \
+ -cshared_buffers=${SHARED}MB -ceffective_cache_size=${CACHE}MB \
+ -cwork_mem=${WORK}MB -cmaintenance_work_mem=${MAINT}MB \
+ -cmax_files_per_process=100 \
+ -cautovacuum=on" start
---
+++ script/stop_timescaledb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo -u postgres /usr/lib/postgresql/9.6/bin/pg_ctl -U postgres -D /etc/postgresql/9.6/main stop
