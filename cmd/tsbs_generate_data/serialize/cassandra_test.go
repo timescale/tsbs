034712881f295fba0902445403d0f0e7925d8533
Lee Hampton <leejhampton@gmail.com>
2018-05-25 17:04:05 -0400

    Encode/decode Cassandra data points in CSV format rather than full CQL statements

---
+++ cmd/tsbs_generate_data/serialize/cassandra_test.go
@@ -0,0 +1,39 @@
+package serialize
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+func TestCassandraSerializerSerialize(t *testing.T) {
+	now := time.Unix(1451606400, 0)
+	cases := []struct {
+		desc       string
+		inputPoint *Point
+		output     string
+	}{
+		{
+			desc: "A Point object should result in CSV output written by the IO Writer",
+			inputPoint: &Point{
+				measurementName: []byte("cpu"),
+				tagKeys:         [][]byte{[]byte("hostname"), []byte("region"), []byte("datacenter")},
+				tagValues:       [][]byte{[]byte("host_0"), []byte("eu-west-1"), []byte("eu-west-1b")},
+				timestamp:       &now,
+				fieldKeys:       [][]byte{[]byte("usage_guest_nice")},
+				fieldValues:     []interface{}{38.2431182911542820},
+			},
+			output: "series_double,cpu,hostname=host_0,region=eu-west-1,datacenter=eu-west-1b,usage_guest_nice,2016-01-01,1451606400000000000,38.2431182911542820\n",
+		},
+	}
+
+	for _, c := range cases {
+		b := new(bytes.Buffer)
+		serializer := &CassandraSerializer{}
+		serializer.Serialize(c.inputPoint, b)
+		got := b.String()
+		if got != c.output {
+			t.Errorf("%s \nOutput incorrect: \nWant: %s \nGot: %s", c.desc, c.output, got)
+		}
+	}
+}
