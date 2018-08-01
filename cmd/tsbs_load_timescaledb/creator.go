0ac2918ab453946baeb03920758e3ea271ddccc0
Rob Kiefer <rob.kiefer@gmail.com>
2018-08-01 17:32:39 -0400

    Fix incorrect header length assumption in TimescaleDB loader
    Previously the loader assumed the header had exactly 3 lines but
    that is only for the cpu-only use case. For the full devops use
    case it has one line per table to create (9) so there are actually
    11 lines.

--- cmd/tsbs_load_timescaledb/creator.go
+++ cmd/tsbs_load_timescaledb/creator.go
@@ -11,7 +11,7 @@ import (
 
 type dbCreator struct {
 	tags    string
-	cols    string
+	cols    []string
 	connStr string
 }
 
@@ -25,28 +25,31 @@ func (d *dbCreator) Init() {
 }
 
 func (d *dbCreator) readDataHeader(br *bufio.Reader) {
-	// First three lines are header, with the first line containing the tags
-	// and their names, the second line containing the column names, and
-	// third line being blank to separate from the data
-	for i := 0; i < 3; i++ {
+	// First N lines are header, with the first line containing the tags
+	// and their names, the second through N-1 line containing the column
+	// names, and last line being blank to separate from the data
+	i := 0
+	for {
 		var err error
-		var empty string
+		var line string
 		if i == 0 {
 			d.tags, err = br.ReadString('\n')
-			d.tags = strings.TrimSpace(d.tags)
-		} else if i == 1 {
-			d.cols, err = br.ReadString('\n')
-			d.cols = strings.TrimSpace(d.cols)
-		} else {
-			empty, err = br.ReadString('\n')
-			empty = strings.TrimSpace(empty)
-			if len(empty) > 0 {
-				fatal("input has wrong header format: third line is not blank")
+			if err != nil {
+				fatal("input has wrong header format: %v", err)
 			}
+			d.tags = strings.TrimSpace(d.tags)
+		} else {
+			line, err = br.ReadString('\n')
+			if err != nil {
+				fatal("input has wrong header format: %v", err)
+			}
+			line = strings.TrimSpace(line)
+			if len(line) == 0 {
+				break
+			}
+			d.cols = append(d.cols, line)
 		}
-		if err != nil {
-			fatal("input has wrong header format: %v", err)
-		}
+		i++
 	}
 }
 
@@ -80,60 +83,62 @@ func (d *dbCreator) CreateDB(dbName string) error {
 	createTagsTable(dbBench, parts[1:])
 	tableCols["tags"] = parts[1:]
 
-	parts = strings.Split(strings.TrimSpace(d.cols), ",")
-	hypertable := parts[0]
-	partitioningField := tableCols["tags"][0]
-	tableCols[hypertable] = parts[1:]
+	for _, cols := range d.cols {
+		parts = strings.Split(strings.TrimSpace(cols), ",")
+		hypertable := parts[0]
+		partitioningField := tableCols["tags"][0]
+		tableCols[hypertable] = parts[1:]
 
-	psuedoCols := []string{}
-	if inTableTag {
-		psuedoCols = append(psuedoCols, partitioningField)
-	}
-
-	fieldDef := []string{}
-	indexes := []string{}
-	psuedoCols = append(psuedoCols, parts[1:]...)
-	extraCols := 0 // set to 1 when hostname is kept in-table
-	for idx, field := range psuedoCols {
-		if len(field) == 0 {
-			continue
-		}
-		fieldType := "DOUBLE PRECISION"
-		idxType := fieldIndex
-		if inTableTag && idx == 0 {
-			fieldType = "TEXT"
-			idxType = ""
-			extraCols = 1
+		psuedoCols := []string{}
+		if inTableTag {
+			psuedoCols = append(psuedoCols, partitioningField)
 		}
 
-		fieldDef = append(fieldDef, fmt.Sprintf("%s %s", field, fieldType))
-		if fieldIndexCount == -1 || idx < (fieldIndexCount+extraCols) {
-			indexes = append(indexes, d.getCreateIndexOnFieldCmds(hypertable, field, idxType)...)
+		fieldDef := []string{}
+		indexes := []string{}
+		psuedoCols = append(psuedoCols, parts[1:]...)
+		extraCols := 0 // set to 1 when hostname is kept in-table
+		for idx, field := range psuedoCols {
+			if len(field) == 0 {
+				continue
+			}
+			fieldType := "DOUBLE PRECISION"
+			idxType := fieldIndex
+			if inTableTag && idx == 0 {
+				fieldType = "TEXT"
+				idxType = ""
+				extraCols = 1
+			}
+
+			fieldDef = append(fieldDef, fmt.Sprintf("%s %s", field, fieldType))
+			if fieldIndexCount == -1 || idx < (fieldIndexCount+extraCols) {
+				indexes = append(indexes, d.getCreateIndexOnFieldCmds(hypertable, field, idxType)...)
+			}
 		}
-	}
-	dbBench.MustExec(fmt.Sprintf("CREATE TABLE %s (time timestamptz, tags_id integer, %s)", hypertable, strings.Join(fieldDef, ",")))
-	if partitionIndex {
-		dbBench.MustExec(fmt.Sprintf("CREATE INDEX ON %s(tags_id, \"time\" DESC)", hypertable))
-	}
+		dbBench.MustExec(fmt.Sprintf("CREATE TABLE %s (time timestamptz, tags_id integer, %s)", hypertable, strings.Join(fieldDef, ",")))
+		if partitionIndex {
+			dbBench.MustExec(fmt.Sprintf("CREATE INDEX ON %s(tags_id, \"time\" DESC)", hypertable))
+		}
 
-	// Only allow one or the other, it's probably never right to have both.
-	// Experimentation suggests (so far) that for 100k devices it is better to
-	// use --time-partition-index for reduced index lock contention.
-	if timePartitionIndex {
-		dbBench.MustExec(fmt.Sprintf("CREATE INDEX ON %s(\"time\" DESC, tags_id)", hypertable))
-	} else if timeIndex {
-		dbBench.MustExec(fmt.Sprintf("CREATE INDEX ON %s(\"time\" DESC)", hypertable))
-	}
+		// Only allow one or the other, it's probably never right to have both.
+		// Experimentation suggests (so far) that for 100k devices it is better to
+		// use --time-partition-index for reduced index lock contention.
+		if timePartitionIndex {
+			dbBench.MustExec(fmt.Sprintf("CREATE INDEX ON %s(\"time\" DESC, tags_id)", hypertable))
+		} else if timeIndex {
+			dbBench.MustExec(fmt.Sprintf("CREATE INDEX ON %s(\"time\" DESC)", hypertable))
+		}
 
-	for _, idxDef := range indexes {
-		dbBench.MustExec(idxDef)
-	}
+		for _, idxDef := range indexes {
+			dbBench.MustExec(idxDef)
+		}
 
-	if useHypertable {
-		dbBench.MustExec("CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE")
-		dbBench.MustExec(
-			fmt.Sprintf("SELECT create_hypertable('%s'::regclass, 'time'::name, partitioning_column => '%s'::name, number_partitions => %v::smallint, chunk_time_interval => %d, create_default_indexes=>FALSE)",
-				hypertable, "tags_id", numberPartitions, chunkTime.Nanoseconds()/1000))
+		if useHypertable {
+			dbBench.MustExec("CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE")
+			dbBench.MustExec(
+				fmt.Sprintf("SELECT create_hypertable('%s'::regclass, 'time'::name, partitioning_column => '%s'::name, number_partitions => %v::smallint, chunk_time_interval => %d, create_default_indexes=>FALSE)",
+					hypertable, "tags_id", numberPartitions, chunkTime.Nanoseconds()/1000))
+		}
 	}
 
 	return nil
