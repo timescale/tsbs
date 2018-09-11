1b3a3046f7937683fb05b22c36cef857be70353b
Vladislav Klimenko <sunsingerus@gmail.com>
2018-09-11 13:43:32 +0300

    Make bash variable naming consistent

--- scripts/load_common.sh
+++ scripts/load_common.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-EXE_DIR=${EXE_DIR:-$(dirname $0)}
-DATABASE_HOST=${DATABASE_HOST:-database}
+# Database credentials
+DATABASE_HOST=${DATABASE_HOST:-"localhost"}
+DATABASE_NAME=${DATABASE_NAME:-"benchmark"}
 
-# load parameters
-BATCH_SIZE=${BATCH_SIZE:-10000}
-BULK_DATA_DIR=${BULK_DATA_DIR:-/tmp/bulk_data}
-NUM_WORKERS=${NUM_WORKERS:-8}  # match # of cores
+# Data folder
+BULK_DATA_DIR=${BULK_DATA_DIR:-"/tmp/bulk_data"}
+# Full path to data file
 DATA_FILE=${DATA_FILE:-${BULK_DATA_DIR}/${DATA_FILE_NAME}}
+
+# Load parameters
+BATCH_SIZE=${BATCH_SIZE:-10000}
+NUM_WORKERS=${NUM_WORKERS:-8}  # match # of cores
 BACKOFF_SECS=${BACKOFF_SECS:-1s}
 
+# Ensure data file is in place
 if [ ! -f ${DATA_FILE} ]; then
    echo "Cannot find data file ${DATA_FILE}"
    exit -1
--- scripts/load_timescaledb.sh
+++ scripts/load_timescaledb.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-EXE_DIR=${EXE_DIR:-$(dirname $0)}
+# Ensure loader is available
+EXE_FILE_NAME=${EXE_FILE_NAME:-$(which tsbs_load_timescaledb)}
+if [[ -z "$EXE_FILE_NAME" ]]; then
+    echo "tsbs_load_timescaledb not available. It is not specified explicitly and not found in \$PATH"
+    exit 1
+fi
+
+# Load parameters - common
 DATA_FILE_NAME=${DATA_FILE_NAME:-timescaledb-data.gz}
+DATABASE_USER=${DATABASE_USER:-postgres}
+
+# Load parameters - personal
 PROGRESS_INTERVAL=${PROGRESS_INTERVAL:-10s}
 CHUNK_TIME=${CHUNK_TIME:-8h}
 PARTITIONS=${PARTITIONS:-1}
 HASH_WORKERS=${HASH_WORKERS:-false}
 TIME_PARTITION_INDEX=${TIME_PARTITION_INDEX:-false}
 PERF_OUTPUT=${PERF_OUTPUT:-}
-DATABASE_HOST=${DATABASE_HOST:-localhost}
-DATABASE_USER=${DATABASE_USER:-postgres}
+JSON_TAGS=${JSON_TAGS:-false}
+IN_TABLE_PARTITION_TAG=${IN_TABLE_PARTITION_TAG:-true}
+USE_HYPERTABLE=${USE_HYPERTABLE:-true}
 
+EXE_DIR=${EXE_DIR:-$(dirname $0)}
 source ${EXE_DIR}/load_common.sh
-source ${EXE_DIR}/timescaledb.conf
 
 while ! pg_isready -h ${DATABASE_HOST}; do
     echo "Waiting for timescaledb"
     sleep 1
 done
 
-cat ${DATA_FILE} | gunzip | tsbs_load_timescaledb \
+cat ${DATA_FILE} | gunzip | $EXE_FILE_NAME \
                                 --postgres="sslmode=disable" \
                                 --db-name=${DATABASE_NAME} \
                                 --host=${DATABASE_HOST} \
