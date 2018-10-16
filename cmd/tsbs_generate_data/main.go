b512318dfb1f30a0446540f135e870cecfc773ce
Rob Kiefer <rob.kiefer@gmail.com>
2018-10-16 15:26:57 -0400

    Add more comprehensive testing for tsbs_generate_data main prog
    Most individual pieces are broken out into testable functions,
    giving us pretty comprehensive coverage for this module even if
    we don't cover init() and main() explicitly.

--- cmd/tsbs_generate_data/main.go
+++ cmd/tsbs_generate_data/main.go
@@ -16,6 +16,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"os"
@@ -41,6 +42,12 @@ const (
 	useCaseCPUOnly   = "cpu-only"
 	useCaseCPUSingle = "cpu-single"
 	useCaseDevops    = "devops"
+
+	errTotalGroupsZero  = "incorrect interleaved groups configuration: total groups = 0"
+	errInvalidGroupsFmt = "incorrect interleaved groups configuration: id %d >= total groups %d"
+	errInvalidFormatFmt = "invalid format specifier: %v (valid choices: %v)"
+
+	inputBufSize = 4 << 20
 )
 
 // semi-constants
@@ -49,6 +56,16 @@ var (
 	// allows for testing
 	fatal = log.Fatalf
 )
+
+// parseableFlagVars are flag values that need sanitization or re-parsing after
+// being set, e.g., to convert from string to time.Time or re-setting the value
+// based on a special '0' value
+type parseableFlagVars struct {
+	timestampStartStr string
+	timestampEndStr   string
+	seed              int64
+	initScaleVar      uint64
+}
 
 // Program option vars:
 var (
@@ -70,103 +87,22 @@ var (
 	logInterval time.Duration
 )
 
-// Parse args:
-func init() {
-	var timestampStartStr string
-	var timestampEndStr string
-	flag.StringVar(&format, "format", "", fmt.Sprintf("Format to emit. (choices: %s)", strings.Join(formatChoices, ", ")))
-
-	flag.StringVar(&useCase, "use-case", "", "Use case to model. (choices: devops, cpu-only)")
-
-	flag.Uint64Var(&initScaleVar, "initial-scale-var", 0, "Initial scaling variable specific to the use case (e.g., devices in 'devops'). 0 means to use -scale-var value")
-	flag.Uint64Var(&scaleVar, "scale-var", 1, "Scaling variable specific to the use case (e.g., devices in 'devops').")
-
-	flag.StringVar(&timestampStartStr, "timestamp-start", "2016-01-01T00:00:00Z", "Beginning timestamp (RFC3339).")
-	flag.StringVar(&timestampEndStr, "timestamp-end", "2016-01-02T06:00:00Z", "Ending timestamp (RFC3339).")
-
-	flag.Int64Var(&seed, "seed", 0, "PRNG seed (0 uses the current timestamp). (default 0)")
-	flag.IntVar(&debug, "debug", 0, "Debug printing (choices: 0, 1, 2). (default 0)")
-
-	flag.UintVar(&interleavedGenerationGroupID, "interleaved-generation-group-id", 0, "Group (0-indexed) to perform round-robin serialization within. Use this to scale up data generation to multiple processes.")
-	flag.UintVar(&interleavedGenerationGroups, "interleaved-generation-groups", 1, "The number of round-robin serialization groups. Use this to scale up data generation to multiple processes.")
-	flag.StringVar(&profileFile, "profile-file", "", "File to which to write go profiling data")
-
-	flag.DurationVar(&logInterval, "log-interval", 10*time.Second, "Duration between host data points")
-	flag.Parse()
-
-	if !(interleavedGenerationGroupID < interleavedGenerationGroups) {
-		log.Fatal("incorrect interleaved groups configuration")
-	}
-
-	if initScaleVar == 0 {
-		initScaleVar = scaleVar
-	}
-
-	// the default seed is the current timestamp:
-	if seed == 0 {
-		seed = int64(time.Now().Nanosecond())
-	}
-	fmt.Fprintf(os.Stderr, "using random seed %d\n", seed)
-
-	// Parse timestamps:
-	var err error
-	timestampStart, err = time.Parse(time.RFC3339, timestampStartStr)
+func parseTimeFromString(s string) time.Time {
+	t, err := time.Parse(time.RFC3339, s)
 	if err != nil {
-		log.Fatal(err)
+		fatal("%v", err)
+		return time.Time{}
 	}
-	timestampStart = timestampStart.UTC()
-	timestampEnd, err = time.Parse(time.RFC3339, timestampEndStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	timestampEnd = timestampEnd.UTC()
+	return t.UTC()
 }
 
-func main() {
-	if !validateFormat(format) {
-		log.Fatal("invalid format specifier")
+func validateGroups(groupID, totalGroups uint) (bool, error) {
+	if totalGroups == 0 {
+		return false, fmt.Errorf(errTotalGroupsZero)
+	} else if groupID >= totalGroups {
+		return false, fmt.Errorf(errInvalidGroupsFmt, groupID, totalGroups)
 	}
-
-	if len(profileFile) > 0 {
-		defer startMemoryProfile(profileFile)()
-	}
-
-	rand.Seed(seed)
-	out := bufio.NewWriterSize(os.Stdout, 4<<20)
-	defer out.Flush()
-
-	cfg := getConfig(useCase)
-	sim := cfg.ToSimulator(logInterval)
-	serializer := getSerializer(sim, format, out)
-
-	currentInterleavedGroup := uint(0)
-	point := serialize.NewPoint()
-	for !sim.Finished() {
-		write := sim.Next(point)
-		if !write {
-			point.Reset()
-			continue
-		}
-
-		// in the default case this is always true
-		if currentInterleavedGroup == interleavedGenerationGroupID {
-			err := serializer.Serialize(point, out)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-		point.Reset()
-
-		currentInterleavedGroup++
-		if currentInterleavedGroup == interleavedGenerationGroups {
-			currentInterleavedGroup = 0
-		}
-	}
-
-	err := out.Flush()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	return true, nil
 }
 
 func validateFormat(format string) bool {
@@ -176,6 +112,105 @@ func validateFormat(format string) bool {
 		}
 	}
 	return false
+}
+
+func postFlagParse(flags parseableFlagVars) {
+	if flags.initScaleVar == 0 {
+		initScaleVar = scaleVar
+	} else {
+		initScaleVar = flags.initScaleVar
+	}
+
+	// the default seed is the current timestamp:
+	if flags.seed == 0 {
+		seed = int64(time.Now().Nanosecond())
+	} else {
+		seed = flags.seed
+	}
+	fmt.Fprintf(os.Stderr, "using random seed %d\n", seed)
+
+	// Parse timestamps
+	timestampStart = parseTimeFromString(flags.timestampStartStr)
+	timestampEnd = parseTimeFromString(flags.timestampEndStr)
+}
+
+// Parse args:
+func init() {
+	pfv := parseableFlagVars{}
+	flag.StringVar(&format, "format", "", fmt.Sprintf("Format to emit. (choices: %s)", strings.Join(formatChoices, ", ")))
+
+	flag.StringVar(&useCase, "use-case", "", "Use case to model. (choices: devops, cpu-only)")
+
+	flag.Uint64Var(&pfv.initScaleVar, "initial-scale-var", 0, "Initial scaling variable specific to the use case (e.g., devices in 'devops'). 0 means to use -scale-var value")
+	flag.Uint64Var(&scaleVar, "scale-var", 1, "Scaling variable specific to the use case (e.g., devices in 'devops').")
+
+	flag.StringVar(&pfv.timestampStartStr, "timestamp-start", "2016-01-01T00:00:00Z", "Beginning timestamp (RFC3339).")
+	flag.StringVar(&pfv.timestampEndStr, "timestamp-end", "2016-01-02T06:00:00Z", "Ending timestamp (RFC3339).")
+
+	flag.Int64Var(&pfv.seed, "seed", 0, "PRNG seed (0 uses the current timestamp). (default 0)")
+
+	flag.IntVar(&debug, "debug", 0, "Debug printing (choices: 0, 1, 2). (default 0)")
+
+	flag.UintVar(&interleavedGenerationGroupID, "interleaved-generation-group-id", 0, "Group (0-indexed) to perform round-robin serialization within. Use this to scale up data generation to multiple processes.")
+	flag.UintVar(&interleavedGenerationGroups, "interleaved-generation-groups", 1, "The number of round-robin serialization groups. Use this to scale up data generation to multiple processes.")
+	flag.StringVar(&profileFile, "profile-file", "", "File to which to write go profiling data")
+
+	flag.DurationVar(&logInterval, "log-interval", 10*time.Second, "Duration between host data points")
+	flag.Parse()
+
+	postFlagParse(pfv)
+}
+
+func main() {
+	if ok, err := validateGroups(interleavedGenerationGroupID, interleavedGenerationGroups); !ok {
+		fatal(err.Error())
+	}
+	if ok := validateFormat(format); !ok {
+		fatal("invalid format specifier: %v (valid choices: %v)", format, formatChoices)
+	}
+
+	if len(profileFile) > 0 {
+		defer startMemoryProfile(profileFile)()
+	}
+
+	rand.Seed(seed)
+	out := bufio.NewWriterSize(os.Stdout, inputBufSize)
+	defer func() {
+		err := out.Flush()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	cfg := getConfig(useCase)
+	sim := cfg.ToSimulator(logInterval)
+	serializer := getSerializer(sim, format, out)
+
+	runSimulator(sim, serializer, out, interleavedGenerationGroupID, interleavedGenerationGroups)
+}
+
+func runSimulator(sim common.Simulator, serializer serialize.PointSerializer, out io.Writer, groupID, totalGroups uint) {
+	currGroup := uint(0)
+	point := serialize.NewPoint()
+	for !sim.Finished() {
+		write := sim.Next(point)
+		if !write {
+			point.Reset()
+			continue
+		}
+
+		// in the default case this is always true
+		if currGroup == groupID {
+			err := serializer.Serialize(point, out)
+			if err != nil {
+				fatal("%v", err)
+				return
+			}
+		}
+		point.Reset()
+
+		currGroup = (currGroup + 1) % totalGroups
+	}
 }
 
 func getConfig(useCase string) common.SimulatorConfig {
--- cmd/tsbs_generate_data/main_test.go
+++ cmd/tsbs_generate_data/main_test.go
@@ -3,11 +3,82 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"fmt"
+	"io"
 	"testing"
+	"time"
 
 	"github.com/timescale/tsbs/cmd/tsbs_generate_data/devops"
 	"github.com/timescale/tsbs/cmd/tsbs_generate_data/serialize"
 )
+
+const (
+	correctTimeStr   = "2016-01-01T00:00:00Z"
+	incorrectTimeStr = "2017-01-01"
+)
+
+var correctTime = time.Date(2016, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+func TestParseTimeFromStrong(t *testing.T) {
+	parsedTime := parseTimeFromString(correctTimeStr)
+	if parsedTime != correctTime {
+		t.Errorf("did not get correct time back: got %v want %v", parsedTime, correctTime)
+	}
+
+	oldFatal := fatal
+	fatalCalled := false
+	fatal = func(format string, args ...interface{}) {
+		fatalCalled = true
+	}
+	_ = parseTimeFromString(incorrectTimeStr)
+	if !fatalCalled {
+		t.Errorf("fatal not called when it should have been")
+	}
+	fatal = oldFatal
+}
+
+func TestValidateGroups(t *testing.T) {
+	cases := []struct {
+		desc        string
+		groupID     uint
+		totalGroups uint
+		shouldErr   bool
+		errFmt      string
+	}{
+		{
+			desc:        "id < total, no err",
+			groupID:     0,
+			totalGroups: 1,
+			shouldErr:   false,
+		},
+		{
+			desc:        "id = total, should err",
+			groupID:     1,
+			totalGroups: 1,
+			shouldErr:   true,
+			errFmt:      errInvalidGroupsFmt,
+		},
+		{
+			desc:        "total = 0, should err",
+			groupID:     0,
+			totalGroups: 0,
+			shouldErr:   true,
+			errFmt:      errTotalGroupsZero,
+		},
+	}
+	for _, c := range cases {
+		ok, err := validateGroups(c.groupID, c.totalGroups)
+		if ok != c.shouldErr {
+			magic := 46 // first 45 chars are the same for both error messages, so check up to 46 to make sure its different
+			if c.shouldErr && err.Error()[:magic] != c.errFmt[:magic] {
+				t.Errorf("%s: did not get correct error: got\n%v\nwant\n%v\n", c.desc, err, c.errFmt)
+			}
+			if !c.shouldErr && err != nil {
+				t.Errorf("%s: got unexpected error: %v", c.desc, err)
+			}
+		}
+	}
+}
 
 func TestValidateFormat(t *testing.T) {
 	for _, f := range formatChoices {
@@ -18,6 +89,227 @@ func TestValidateFormat(t *testing.T) {
 	if validateFormat("incorrect format!") {
 		t.Errorf("validateFormat returned true for invalid format")
 	}
+}
+
+func TestPostFlagsParse(t *testing.T) {
+	scaleVar = 100
+	timestampStart = time.Time{}
+	timestampEnd = time.Time{}
+	boringPFV := parseableFlagVars{
+		initScaleVar:      1,
+		seed:              123,
+		timestampStartStr: correctTimeStr,
+		timestampEndStr:   correctTimeStr,
+	}
+	postFlagParse(boringPFV)
+	if initScaleVar != boringPFV.initScaleVar {
+		t.Errorf("specified initScaleVar not set correctly: got %d", initScaleVar)
+	}
+	if seed != boringPFV.seed {
+		t.Errorf("specified seed not set correctly: got %d", seed)
+	}
+	if timestampStart != correctTime {
+		t.Errorf("start time not parsed correctly: got %v", timestampStart)
+	}
+	if timestampEnd != correctTime {
+		t.Errorf("end time not parsed correctly: got %v", timestampEnd)
+	}
+
+	// initScaleVar should set to the same as scaleVar
+	testPFV := parseableFlagVars{
+		initScaleVar:      0,
+		seed:              boringPFV.seed,
+		timestampStartStr: boringPFV.timestampStartStr,
+		timestampEndStr:   boringPFV.timestampEndStr,
+	}
+	postFlagParse(testPFV)
+	if initScaleVar != scaleVar {
+		t.Errorf("initScaleVar = 0 not parsed correctly: got %d", initScaleVar)
+	}
+
+	// seed should set to current time
+	testPFV = parseableFlagVars{
+		initScaleVar:      boringPFV.initScaleVar,
+		seed:              0,
+		timestampStartStr: boringPFV.timestampStartStr,
+		timestampEndStr:   boringPFV.timestampEndStr,
+	}
+	postFlagParse(testPFV)
+	if seed == boringPFV.seed || seed == 0 {
+		t.Errorf("seed = 0 not parsed correctly: got %d", seed)
+	}
+
+	// check that incorrect times fail
+	oldFatal := fatal
+	fatalCalled := false
+	fatal = func(format string, args ...interface{}) {
+		fatalCalled = true
+	}
+	testPFV = parseableFlagVars{
+		initScaleVar:      boringPFV.initScaleVar,
+		seed:              boringPFV.seed,
+		timestampStartStr: incorrectTimeStr,
+		timestampEndStr:   boringPFV.timestampEndStr,
+	}
+	postFlagParse(testPFV)
+	if !fatalCalled {
+		t.Errorf("fatal not called when it should have been")
+	}
+
+	testPFV = parseableFlagVars{
+		initScaleVar:      boringPFV.initScaleVar,
+		seed:              boringPFV.seed,
+		timestampStartStr: boringPFV.timestampStartStr,
+		timestampEndStr:   incorrectTimeStr,
+	}
+	postFlagParse(testPFV)
+	if !fatalCalled {
+		t.Errorf("fatal not called when it should have been")
+	}
+	fatal = oldFatal
+}
+
+var keyIteration = []byte("iteration")
+
+type testSimulator struct {
+	limit            uint64
+	shouldWriteLimit uint64
+	iteration        uint64
+}
+
+func (s *testSimulator) Finished() bool {
+	return s.iteration >= s.limit
+}
+
+func (s *testSimulator) Next(p *serialize.Point) bool {
+	p.AppendField(keyIteration, s.iteration)
+	ret := s.iteration < s.shouldWriteLimit
+	s.iteration++
+	return ret
+}
+
+func (s *testSimulator) Fields() map[string][][]byte {
+	return nil
+}
+
+type testSerializer struct {
+	shouldError bool
+}
+
+func (s *testSerializer) Serialize(p *serialize.Point, w io.Writer) error {
+	if s.shouldError {
+		return fmt.Errorf("erroring")
+	}
+	w.Write(keyIteration)
+	w.Write([]byte("="))
+	str := fmt.Sprintf("%d", p.GetFieldValue(keyIteration).(uint64))
+	w.Write([]byte(str))
+	w.Write([]byte("\n"))
+	return nil
+}
+
+func TestRunSimulator(t *testing.T) {
+	cases := []struct {
+		desc             string
+		limit            uint64
+		shouldWriteLimit uint64
+		groupID          uint
+		totalGroups      uint
+		shouldError      bool
+		wantPoints       uint
+	}{
+		{
+			desc:             "shouldWriteLimit = limit",
+			limit:            10,
+			shouldWriteLimit: 10,
+			totalGroups:      1,
+			wantPoints:       10,
+		},
+		{
+			desc:             "shouldWriteLimit < limit",
+			limit:            10,
+			shouldWriteLimit: 5,
+			totalGroups:      1,
+			wantPoints:       5,
+		},
+		{
+			desc:             "shouldWriteLimit > limit",
+			limit:            10,
+			shouldWriteLimit: 15,
+			totalGroups:      1,
+			wantPoints:       10,
+		},
+		{
+			desc:             "shouldWriteLimit = limit, totalGroups=2",
+			limit:            10,
+			shouldWriteLimit: 10,
+			totalGroups:      2,
+			wantPoints:       5,
+		},
+		{
+			desc:             "shouldWriteLimit < limit, totalGroups=2",
+			limit:            10,
+			shouldWriteLimit: 6,
+			totalGroups:      2,
+			wantPoints:       3,
+		},
+		{
+			desc:             "shouldWriteLimit < limit, totalGroups=2, other half",
+			limit:            10,
+			shouldWriteLimit: 6,
+			groupID:          1,
+			totalGroups:      2,
+			wantPoints:       3,
+		},
+		{
+			desc:             "should error in serializer",
+			shouldError:      true,
+			limit:            10,
+			totalGroups:      1,
+			shouldWriteLimit: 10,
+		},
+	}
+	oldFatal := fatal
+	for _, c := range cases {
+		fatalCalled := false
+		if c.shouldError {
+			fatal = func(format string, args ...interface{}) {
+				fatalCalled = true
+			}
+		}
+		var buf bytes.Buffer
+		sim := &testSimulator{
+			limit:            c.limit,
+			shouldWriteLimit: c.shouldWriteLimit,
+		}
+		serializer := &testSerializer{shouldError: c.shouldError}
+
+		runSimulator(sim, serializer, &buf, c.groupID, c.totalGroups)
+		if c.shouldError && !fatalCalled {
+			t.Errorf("%s: did not fatal when should", c.desc)
+		} else if !c.shouldError {
+			scanner := bufio.NewScanner(bytes.NewReader(buf.Bytes()))
+			lines := uint(0)
+			for {
+				ok := scanner.Scan()
+				if !ok && scanner.Err() != nil {
+					t.Fatal(scanner.Err())
+				} else if !ok {
+					break
+				}
+				line := scanner.Text()
+				want := fmt.Sprintf("iteration=%d", (lines*c.totalGroups)+c.groupID)
+				if line != want {
+					t.Errorf("%s: incorrect line: got\n%s\nwant\n%s\n", c.desc, line, want)
+				}
+				lines++
+			}
+			if lines != c.wantPoints {
+				t.Errorf("%s: incorrect number of points: got %d want %d", c.desc, lines, c.wantPoints)
+			}
+		}
+	}
+	fatal = oldFatal
 }
 
 func TestGetConfig(t *testing.T) {
@@ -42,6 +334,7 @@ func TestGetConfig(t *testing.T) {
 		t.Errorf("use case '%s' does not run the right type: got %T", useCaseDevops, got)
 	}
 
+	oldFatal := fatal
 	fatalCalled := false
 	fatal = func(f string, args ...interface{}) {
 		fatalCalled = true
@@ -53,6 +346,7 @@ func TestGetConfig(t *testing.T) {
 	if cfg != nil {
 		t.Errorf("got a non-nil config for bogus use case: got %T", cfg)
 	}
+	fatal = oldFatal
 }
 
 func TestGetSerializer(t *testing.T) {
@@ -90,6 +384,7 @@ func TestGetSerializer(t *testing.T) {
 		t.Errorf("format '%s' does not run the right serializer: got %T", formatTimescaleDB, got)
 	}
 
+	oldFatal := fatal
 	fatalCalled := false
 	fatal = func(f string, args ...interface{}) {
 		fatalCalled = true
@@ -101,4 +396,5 @@ func TestGetSerializer(t *testing.T) {
 	if s != nil {
 		t.Errorf("got a non-nil config for bogus format: got %T", cfg)
 	}
+	fatal = oldFatal
 }
