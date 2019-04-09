feb417e061b5fddef47a7e70bfa06a9df0dffe29
Rob Kiefer <rob.kiefer@gmail.com>
2019-04-09 15:36:13 -0400

    Rewrite tsbs_generate_queries to use internal/inputs
    This PR continues on the work in the previous one that changed
    tsbs_generate_data to use a new internal/inputs package. This PR
    adds a new Generator type for query generation called QueryGenerator.
    
    Now that these two generators share some common code, they both
    become much more robust and easier to test and manage. Previously
    tsbs_generate_queries had no test coverage, but with this change it
    will actually have quite high coverage.
    
    There are still some rough spots with this refactor. In particular,
    how the useCaseMatrix is handled needs some more thought, especially
    if we are going to add more use cases going forward. Additionally,
    the database specific flags like TimescaleUseJSON could probably
    be handled in a cleaner way as well.

--- cmd/tsbs_generate_queries/main.go
+++ cmd/tsbs_generate_queries/main.go
@@ -3,24 +3,13 @@
 package main
 
 import (
-	"bufio"
-	"encoding/gob"
 	"flag"
 	"fmt"
-	"log"
-	"math/rand"
 	"os"
-	"sort"
-	"time"
 
-	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/cassandra"
-	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/clickhouse"
-	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/influx"
-	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/mongo"
-	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/siridb"
-	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/timescaledb"
 	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/uses/devops"
 	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/utils"
+	"github.com/timescale/tsbs/internal/inputs"
 )
 
 var useCaseMatrix = map[string]map[string]utils.QueryFillerMaker{
@@ -43,72 +32,7 @@ var useCaseMatrix = map[string]map[string]utils.QueryFillerMaker{
 	},
 }
 
-const defaultWriteSize = 4 << 20 // 4 MB
-
-// Program option vars:
-var (
-	fatal = log.Fatalf
-
-	generator utils.DevopsGenerator
-	filler    utils.QueryFiller
-
-	queryCount int
-	fileName   string
-
-	seed  int64
-	debug int
-
-	timescaleUseJSON       bool
-	timescaleUseTags       bool
-	timescaleUseTimeBucket bool
-
-	clickhouseUseTags bool
-
-	interleavedGenerationGroupID uint
-	interleavedGenerationGroups  uint
-)
-
-func getGenerator(format string, start, end time.Time, scale int) utils.DevopsGenerator {
-	if format == "cassandra" {
-		return cassandra.NewDevops(start, end, scale)
-	} else if format == "clickhouse" {
-		tgen := clickhouse.NewDevops(start, end, scale)
-		tgen.UseTags = clickhouseUseTags
-		return tgen
-	} else if format == "influx" {
-		return influx.NewDevops(start, end, scale)
-	} else if format == "mongo" {
-		return mongo.NewDevops(start, end, scale)
-	} else if format == "mongo-naive" {
-		return mongo.NewNaiveDevops(start, end, scale)
-	} else if format == "siridb" {
-		return siridb.NewDevops(start, end, scale)
-	} else if format == "timescaledb" {
-		tgen := timescaledb.NewDevops(start, end, scale)
-		tgen.UseJSON = timescaleUseJSON
-		tgen.UseTags = timescaleUseTags
-		tgen.UseTimeBucket = timescaleUseTimeBucket
-		return tgen
-	}
-
-	panic(fmt.Sprintf("no devops generator specified for format '%s'", format))
-}
-
-// GetBufferedWriter returns the buffered Writer that should be used for generated output
-func GetBufferedWriter(fileName string) *bufio.Writer {
-	// Prepare output file/STDOUT
-	if len(fileName) > 0 {
-		// Write output to file
-		file, err := os.Create(fileName)
-		if err != nil {
-			fatal("cannot open file for write %s: %v", fileName, err)
-		}
-		return bufio.NewWriterSize(file, defaultWriteSize)
-	}
-
-	// Write output to STDOUT
-	return bufio.NewWriterSize(os.Stdout, defaultWriteSize)
-}
+var config = &inputs.QueryGeneratorConfig{}
 
 // Parse args:
 func init() {
@@ -127,141 +51,23 @@ func init() {
 		}
 	}
 
-	var format string
-	var useCase string
-	var queryType string
-	var scale int
-	var timestampStartStr string
-	var timestampEndStr string
+	config.AddToFlagSet(flag.CommandLine)
 
-	flag.StringVar(&format, "format", "", "Format to emit. (Choices are in the use case matrix.)")
-	flag.StringVar(&useCase, "use-case", "", "Use case to model. (Choices are in the use case matrix.)")
-	flag.StringVar(&queryType, "query-type", "", "Query type. (Choices are in the use case matrix.)")
+	flag.Uint64Var(&config.Limit, "queries", 1000, "Number of queries to generate.")
 
-	flag.IntVar(&scale, "scale", 1, "Scaling variable (must be the equal to the scalevar used for data generation).")
-	flag.IntVar(&queryCount, "queries", 1000, "Number of queries to generate.")
-
-	flag.BoolVar(&timescaleUseJSON, "timescale-use-json", false, "TimescaleDB only: Use separate JSON tags table when querying")
-	flag.BoolVar(&timescaleUseTags, "timescale-use-tags", true, "TimescaleDB only: Use separate tags table when querying")
-	flag.BoolVar(&timescaleUseTimeBucket, "timescale-use-time-bucket", true, "TimescaleDB only: Use time bucket. Set to false to test on native PostgreSQL")
-
-	flag.BoolVar(&clickhouseUseTags, "clickhouse-use-tags", true, "ClickHouse only: Use separate tags table when querying")
-
-	flag.StringVar(&timestampStartStr, "timestamp-start", "2016-01-01T00:00:00Z", "Beginning timestamp (RFC3339).")
-	flag.StringVar(&timestampEndStr, "timestamp-end", "2016-01-02T06:00:00Z", "Ending timestamp (RFC3339).")
-
-	flag.Int64Var(&seed, "seed", 0, "PRNG seed (default, or 0, uses the current timestamp).")
-	flag.IntVar(&debug, "debug", 0, "Debug printing (choices: 0, 1) (default 0).")
-
-	flag.UintVar(&interleavedGenerationGroupID, "interleaved-generation-group-id", 0, "Group (0-indexed) to perform round-robin serialization within. Use this to scale up data generation to multiple processes.")
-	flag.UintVar(&interleavedGenerationGroups, "interleaved-generation-groups", 1, "The number of round-robin serialization groups. Use this to scale up data generation to multiple processes.")
-
-	flag.StringVar(&fileName, "file", "", "File name to write generated queries to")
+	flag.BoolVar(&config.ClickhouseUseTags, "clickhouse-use-tags", true, "ClickHouse only: Use separate tags table when querying")
+	flag.BoolVar(&config.MongoUseNaive, "mongo-use-naive", true, "MongoDB only: Generate queries for the 'naive' data storage format for Mongo")
+	flag.BoolVar(&config.TimescaleUseJSON, "timescale-use-json", false, "TimescaleDB only: Use separate JSON tags table when querying")
+	flag.BoolVar(&config.TimescaleUseTags, "timescale-use-tags", true, "TimescaleDB only: Use separate tags table when querying")
+	flag.BoolVar(&config.TimescaleUseTimeBucket, "timescale-use-time-bucket", true, "TimescaleDB only: Use time bucket. Set to false to test on native PostgreSQL")
 
 	flag.Parse()
-
-	if !(interleavedGenerationGroupID < interleavedGenerationGroups) {
-		fatal("incorrect interleaved groups configuration")
-	}
-
-	if _, ok := useCaseMatrix[useCase]; !ok {
-		fatal("invalid use case specifier: '%s'", useCase)
-	}
-
-	if _, ok := useCaseMatrix[useCase][queryType]; !ok {
-		fatal("invalid query type specifier: '%s'", queryType)
-	}
-
-	// the default seed is the current timestamp:
-	if seed == 0 {
-		seed = int64(time.Now().Nanosecond())
-	}
-	fmt.Fprintf(os.Stderr, "using random seed %d\n", seed)
-
-	// Parse timestamps:
-	var err error
-	timestampStart, err := time.Parse(time.RFC3339, timestampStartStr)
-	if err != nil {
-		fatal(err.Error())
-	}
-	timestampStart = timestampStart.UTC()
-	timestampEnd, err := time.Parse(time.RFC3339, timestampEndStr)
-	if err != nil {
-		fatal(err.Error())
-	}
-	timestampEnd = timestampEnd.UTC()
-
-	// Make the query generator:
-	generator = getGenerator(format, timestampStart, timestampEnd, scale)
-	filler = useCaseMatrix[useCase][queryType](generator)
 }
 
 func main() {
-	rand.Seed(seed)
-	// Set up bookkeeping:
-	stats := make(map[string]int64)
-
-	// Get output writer
-	out := GetBufferedWriter(fileName)
-	defer func() {
-		err := out.Flush()
-		if err != nil {
-			fatal(err.Error())
-		}
-	}()
-
-	// Create request instances, serializing them to stdout and collecting
-	// counts for each kind. If applicable, only prints queries that
-	// belong to this interleaved group id:
-	currentInterleavedGroup := uint(0)
-
-	enc := gob.NewEncoder(out)
-	for i := 0; i < queryCount; i++ {
-		q := generator.GenerateEmptyQuery()
-		q = filler.Fill(q)
-
-		if currentInterleavedGroup == interleavedGenerationGroupID {
-			err := enc.Encode(q)
-			if err != nil {
-				fatal("encoder %v", err)
-			}
-			stats[string(q.HumanLabelName())]++
-
-			if debug == 1 {
-				_, err := fmt.Fprintf(os.Stderr, "%s\n", q.HumanLabelName())
-				if err != nil {
-					fatal(err.Error())
-				}
-			} else if debug == 2 {
-				_, err := fmt.Fprintf(os.Stderr, "%s\n", q.HumanDescriptionName())
-				if err != nil {
-					fatal(err.Error())
-				}
-			} else if debug >= 3 {
-				_, err := fmt.Fprintf(os.Stderr, "%s\n", q.String())
-				if err != nil {
-					fatal(err.Error())
-				}
-			}
-		}
-		q.Release()
-
-		currentInterleavedGroup++
-		if currentInterleavedGroup == interleavedGenerationGroups {
-			currentInterleavedGroup = 0
-		}
-	}
-
-	// Print stats:
-	keys := []string{}
-	for k := range stats {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	for _, k := range keys {
-		_, err := fmt.Fprintf(os.Stderr, "%s: %d points\n", k, stats[k])
-		if err != nil {
-			fatal(err.Error())
-		}
+	qg := inputs.NewQueryGenerator(useCaseMatrix)
+	err := qg.Generate(config)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
 	}
 }
---
+++ internal/inputs/generator_queries.go
@@ -0,0 +1,272 @@
+package inputs
+
+import (
+	"bufio"
+	"encoding/gob"
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"sort"
+	"time"
+
+	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/cassandra"
+	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/clickhouse"
+	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/influx"
+	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/mongo"
+	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/siridb"
+	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/databases/timescaledb"
+	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/utils"
+)
+
+// Error messages when using a QueryGenerator
+const (
+	ErrInvalidQueryConfig = "invalid config: QueryGenerator needs a QueryGeneratorConfig"
+	ErrEmptyQueryType     = "query type cannot be empty"
+
+	errBadQueryTypeFmt        = "invalid query type for use case '%s': '%s'"
+	errCouldNotDebugFmt       = "could not write debug output: %v"
+	errCouldNotEncodeQueryFmt = "could not encode query: %v"
+	errCouldNotQueryStatsFmt  = "could not output query stats: %v"
+)
+
+// QueryGeneratorConfig is the GeneratorConfig that should be used with a
+// QueryGenerator. It includes all the fields from a BaseConfig, as well as
+// options that are specific to generating the queries to test against a
+// database, such as the query type and individual database options.
+type QueryGeneratorConfig struct {
+	BaseConfig
+	QueryType            string
+	InterleavedGroupID   uint
+	InterleavedNumGroups uint
+
+	// TODO - I think this needs some rethinking, but a simple, elegant solution escapes me right now
+	TimescaleUseJSON       bool
+	TimescaleUseTags       bool
+	TimescaleUseTimeBucket bool
+
+	ClickhouseUseTags bool
+
+	MongoUseNaive bool
+}
+
+// Validate checks that the values of the QueryGeneratorConfig are reasonable.
+func (c *QueryGeneratorConfig) Validate() error {
+	err := c.BaseConfig.Validate()
+	if err != nil {
+		return err
+	}
+
+	if c.QueryType == "" {
+		return fmt.Errorf(ErrEmptyQueryType)
+	}
+
+	err = validateGroups(c.InterleavedGroupID, c.InterleavedNumGroups)
+	return err
+}
+
+func (c *QueryGeneratorConfig) AddToFlagSet(fs *flag.FlagSet) {
+	c.BaseConfig.AddToFlagSet(fs)
+	flag.StringVar(&c.QueryType, "query-type", "", "Query type. (Choices are in the use case matrix.)")
+
+	flag.UintVar(&c.InterleavedGroupID, "interleaved-generation-group-id", 0,
+		"Group (0-indexed) to perform round-robin serialization within. Use this to scale up data generation to multiple processes.")
+	flag.UintVar(&c.InterleavedNumGroups, "interleaved-generation-groups", 1,
+		"The number of round-robin serialization groups. Use this to scale up data generation to multiple processes.")
+
+}
+
+// QueryGenerator is a type of Generator for creating queries to test against a
+// database. The output is specific to the type of database (due to each using
+// different querying techniques, e.g. SQL or REST), but is consumed by TSBS
+// query runners like tsbs_run_queries_timescaledb.
+type QueryGenerator struct {
+	// Out is the writer where data should be written. If nil, it will be
+	// os.Stdout unless File is specified in the GeneratorConfig passed to
+	// Generate.
+	Out io.Writer
+	// DebugOut is where non-generated messages should be written. If nil, it
+	// will be os.Stderr.
+	DebugOut io.Writer
+
+	config        *QueryGeneratorConfig
+	useCaseMatrix map[string]map[string]utils.QueryFillerMaker
+	tsStart       time.Time
+	tsEnd         time.Time
+
+	// bufOut represents the buffered writer that should actually be passed to
+	// any operations that write out data.
+	bufOut *bufio.Writer
+}
+
+// NewQueryGenerator returns a QueryGenerator that is set up to work with a given
+// useCaseMatrix, which tells it how to generate the given query type for a use
+// case.
+func NewQueryGenerator(useCaseMatrix map[string]map[string]utils.QueryFillerMaker) *QueryGenerator {
+	return &QueryGenerator{
+		useCaseMatrix: useCaseMatrix,
+	}
+}
+
+func (g *QueryGenerator) Generate(config GeneratorConfig) error {
+	err := g.init(config)
+	if err != nil {
+		return err
+	}
+
+	useGen, err := g.getUseCaseGenerator(g.config)
+	if err != nil {
+		return err
+	}
+
+	filler := g.useCaseMatrix[g.config.Use][g.config.QueryType](useGen)
+
+	return g.runQueryGeneration(useGen, filler, g.config)
+}
+
+func (g *QueryGenerator) init(config GeneratorConfig) error {
+	if config == nil {
+		return fmt.Errorf(ErrNoConfig)
+	}
+	switch config.(type) {
+	case *QueryGeneratorConfig:
+	default:
+		return fmt.Errorf(ErrInvalidDataConfig)
+	}
+	g.config = config.(*QueryGeneratorConfig)
+
+	err := g.config.Validate()
+	if err != nil {
+		return err
+	}
+
+	if _, ok := g.useCaseMatrix[g.config.Use]; !ok {
+		return fmt.Errorf(errBadUseFmt, g.config.Use)
+	}
+
+	if _, ok := g.useCaseMatrix[g.config.Use][g.config.QueryType]; !ok {
+		return fmt.Errorf(errBadQueryTypeFmt, g.config.Use, g.config.QueryType)
+	}
+
+	g.tsStart, err = ParseUTCTime(g.config.TimeStart)
+	if err != nil {
+		return fmt.Errorf(errCannotParseTimeFmt, g.config.TimeStart, err)
+	}
+	g.tsEnd, err = ParseUTCTime(g.config.TimeEnd)
+	if err != nil {
+		return fmt.Errorf(errCannotParseTimeFmt, g.config.TimeEnd, err)
+	}
+
+	if g.Out == nil {
+		g.Out = os.Stdout
+	}
+	g.bufOut, err = getBufferedWriter(g.config.File, g.Out)
+	if err != nil {
+		return err
+	}
+
+	if g.DebugOut == nil {
+		g.DebugOut = os.Stderr
+	}
+
+	return nil
+}
+
+func (g *QueryGenerator) getUseCaseGenerator(c *QueryGeneratorConfig) (utils.DevopsGenerator, error) {
+	var ret utils.DevopsGenerator
+	scale := int(c.Scale) // TODO: make all the Devops constructors use a uint64
+
+	switch c.Format {
+	case FormatCassandra:
+		ret = cassandra.NewDevops(g.tsStart, g.tsEnd, scale)
+	case FormatClickhouse:
+		temp := clickhouse.NewDevops(g.tsStart, g.tsEnd, scale)
+		temp.UseTags = c.ClickhouseUseTags
+		ret = temp
+	case FormatInflux:
+		ret = influx.NewDevops(g.tsStart, g.tsEnd, scale)
+	case FormatMongo:
+		if c.MongoUseNaive {
+			ret = mongo.NewNaiveDevops(g.tsStart, g.tsEnd, scale)
+		} else {
+			ret = mongo.NewDevops(g.tsStart, g.tsEnd, scale)
+		}
+	case FormatSiriDB:
+		ret = siridb.NewDevops(g.tsStart, g.tsEnd, scale)
+	case FormatTimescaleDB:
+		temp := timescaledb.NewDevops(g.tsStart, g.tsEnd, scale)
+		temp.UseJSON = c.TimescaleUseJSON
+		temp.UseTags = c.TimescaleUseTags
+		temp.UseTimeBucket = c.TimescaleUseTimeBucket
+		ret = temp
+	default:
+		return nil, fmt.Errorf(errUnknownFormatFmt, c.Format)
+	}
+	return ret, nil
+}
+
+func (g *QueryGenerator) runQueryGeneration(useGen utils.DevopsGenerator, filler utils.QueryFiller, c *QueryGeneratorConfig) error {
+	stats := make(map[string]int64)
+	currentGroup := uint(0)
+	enc := gob.NewEncoder(g.bufOut)
+	defer g.bufOut.Flush()
+
+	rand.Seed(g.config.Seed)
+	//fmt.Println(g.config.Seed)
+	if g.config.Debug > 0 {
+		_, err := fmt.Fprintf(g.DebugOut, "using random seed %d\n", g.config.Seed)
+		if err != nil {
+			return fmt.Errorf(errCouldNotDebugFmt, err)
+		}
+	}
+
+	for i := 0; i < int(c.Limit); i++ {
+		q := useGen.GenerateEmptyQuery()
+		q = filler.Fill(q)
+
+		if currentGroup == c.InterleavedGroupID {
+			err := enc.Encode(q)
+			if err != nil {
+				return fmt.Errorf(errCouldNotEncodeQueryFmt, err)
+			}
+			stats[string(q.HumanLabelName())]++
+
+			if c.Debug > 0 {
+				var debugMsg string
+				if c.Debug == 1 {
+					debugMsg = string(q.HumanLabelName())
+				} else if c.Debug == 2 {
+					debugMsg = string(q.HumanDescriptionName())
+				} else if c.Debug >= 3 {
+					debugMsg = q.String()
+				}
+
+				_, err = fmt.Fprintf(g.DebugOut, debugMsg+"\n")
+				if err != nil {
+					return fmt.Errorf(errCouldNotDebugFmt, err)
+				}
+			}
+		}
+		q.Release()
+
+		currentGroup++
+		if currentGroup == c.InterleavedNumGroups {
+			currentGroup = 0
+		}
+	}
+
+	// Print stats:
+	keys := []string{}
+	for k := range stats {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		_, err := fmt.Fprintf(g.DebugOut, "%s: %d points\n", k, stats[k])
+		if err != nil {
+			return fmt.Errorf(errCouldNotQueryStatsFmt, err)
+		}
+	}
+	return nil
+}
