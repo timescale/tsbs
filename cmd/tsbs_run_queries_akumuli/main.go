f3ac7e50416d1ec7dc1ac4758bdc1dc71a156ea5
Evgeny Lazin <4lazin@gmail.com>
2019-05-17 03:45:43 -0700

    Add query runner for Akumuli

---
+++ cmd/tsbs_run_queries_akumuli/main.go
@@ -0,0 +1,70 @@
+// tsbs_run_queries_akumuli speed tests Akumuli using requests from stdin.
+//
+// It reads encoded Query objects from stdin, and makes concurrent requests
+// to the provided HTTP endpoint. This program has no knowledge of the
+// internals of the endpoint.
+package main
+
+import (
+	"flag"
+	"log"
+	"strings"
+
+	"github.com/timescale/tsbs/query"
+)
+
+// Program option vars:
+var (
+	daemonUrls []string
+)
+
+// Global vars:
+var (
+	runner *query.BenchmarkRunner
+)
+
+// Parse args:
+func init() {
+	runner = query.NewBenchmarkRunner()
+	var csvDaemonUrls string
+
+	flag.StringVar(&csvDaemonUrls, "urls", "http://localhost:8181", "Daemon URLs, comma-separated. Will be used in a round-robin fashion.")
+
+	flag.Parse()
+
+	daemonUrls = strings.Split(csvDaemonUrls, ",")
+	if len(daemonUrls) == 0 {
+		log.Fatal("missing 'urls' flag")
+	}
+}
+
+func main() {
+	runner.Run(&query.HTTPPool, newProcessor)
+}
+
+type processor struct {
+	w    *HTTPClient
+	opts *HTTPClientDoOptions
+}
+
+func newProcessor() query.Processor { return &processor{} }
+
+func (p *processor) Init(workerNumber int) {
+	p.opts = &HTTPClientDoOptions{
+		Debug:          runner.DebugLevel(),
+		PrintResponses: runner.DoPrintResponses(),
+	}
+	url := daemonUrls[workerNumber%len(daemonUrls)]
+	p.w = NewHTTPClient(url)
+}
+
+func (p *processor) ProcessQuery(q query.Query, _ bool) ([]*query.Stat, error) {
+	hq := q.(*query.HTTP)
+	lag, err := p.w.Do(hq, p.opts)
+	if err != nil {
+		return nil, err
+	}
+	stat := query.GetStat()
+	stat.Init(q.HumanLabelName(), lag)
+	return []*query.Stat{stat}, nil
+}
