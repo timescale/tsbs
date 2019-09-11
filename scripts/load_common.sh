5c08e3e367d8f85033bf0558d7bbac6a4173dac3
niksa <niksa@timescale.com>
2019-09-11 09:35:44 +0200

    Add port argument. Fix max_queries script

--- scripts/load_common.sh
+++ scripts/load_common.sh
@@ -3,6 +3,7 @@
 # Database credentials
 DATABASE_HOST=${DATABASE_HOST:-"localhost"}
 DATABASE_NAME=${DATABASE_NAME:-"benchmark"}
+PORT=${PORT:-5432}
 
 # Data folder
 BULK_DATA_DIR=${BULK_DATA_DIR:-"/tmp/bulk_data"}
--- scripts/load_timescaledb.sh
+++ scripts/load_timescaledb.sh
@@ -30,7 +30,7 @@ PARTITION_ON_HOSTNAME=${PARTITION_ON_HOSTNAME:-false}
 EXE_DIR=${EXE_DIR:-$(dirname $0)}
 source ${EXE_DIR}/load_common.sh
 
-while ! pg_isready -h ${DATABASE_HOST}; do
+while ! pg_isready -h ${DATABASE_HOST} -p ${PORT}; do
     echo "Waiting for timescaledb"
     sleep 1
 done
@@ -39,6 +39,7 @@ cat ${DATA_FILE} | gunzip | $EXE_FILE_NAME \
                                 --postgres="sslmode=disable" \
                                 --db-name=${DATABASE_NAME} \
                                 --host=${DATABASE_HOST} \
+                                --port=${PORT} \
                                 --user=${DATABASE_USER} \
                                 --workers=${NUM_WORKERS} \
                                 --batch-size=${BATCH_SIZE} \
