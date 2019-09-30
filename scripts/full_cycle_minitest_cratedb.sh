b8a4ea3c88ec9ed3302a70e02972c7d59fe532ab
hagen1778 <hagen1778@gmail.com>
2019-09-30 12:07:00 +0200

    fix the way to setting flags in scripts due to switching to viper lib

--- scripts/full_cycle_minitest_cratedb.sh
+++ scripts/full_cycle_minitest_cratedb.sh
@@ -2,14 +2,14 @@
 
 # generate data
 mkdir -p /tmp/bulk_data
-$GOPATH/bin/tsbs_generate_data -format cratedb -use-case cpu-only -scale 10 -seed 123 -file /tmp/bulk_data/cratedb_data
+$GOPATH/bin/tsbs_generate_data --format cratedb --use-case cpu-only --scale 10 --seed 123 --file /tmp/bulk_data/cratedb_data
 
 # generate queries
-$GOPATH/bin/tsbs_generate_queries -format cratedb -use-case cpu-only -scale 10 -seed 123 -query-type lastpoint             -file /tmp/bulk_data/cratedb_query_lastpoint
-$GOPATH/bin/tsbs_generate_queries -format cratedb -use-case cpu-only -scale 10 -seed 123 -query-type cpu-max-all-1         -file /tmp/bulk_data/cratedb_query_cpu-max-all-1
-$GOPATH/bin/tsbs_generate_queries -format cratedb -use-case cpu-only -scale 10 -seed 123 -query-type high-cpu-1            -file /tmp/bulk_data/cratedb_query_high-cpu-1
-$GOPATH/bin/tsbs_generate_queries -format cratedb -use-case cpu-only -scale 10 -seed 123 -query-type single-groupby-5-1-1  -file /tmp/bulk_data/cratedb_query_single-groupby-5-1-1
-$GOPATH/bin/tsbs_generate_queries -format cratedb -use-case cpu-only -scale 10 -seed 123 -query-type groupby-orderby-limit -file /tmp/bulk_data/cratedb_query_groupby-orderby-limit
+$GOPATH/bin/tsbs_generate_queries --format cratedb --use-case cpu-only --scale 10 --seed 123 --query-type lastpoint             --file /tmp/bulk_data/cratedb_query_lastpoint
+$GOPATH/bin/tsbs_generate_queries --format cratedb --use-case cpu-only --scale 10 --seed 123 --query-type cpu-max-all-1         --file /tmp/bulk_data/cratedb_query_cpu-max-all-1
+$GOPATH/bin/tsbs_generate_queries --format cratedb --use-case cpu-only --scale 10 --seed 123 --query-type high-cpu-1            --file /tmp/bulk_data/cratedb_query_high-cpu-1
+$GOPATH/bin/tsbs_generate_queries --format cratedb --use-case cpu-only --scale 10 --seed 123 --query-type single-groupby-5-1-1  --file /tmp/bulk_data/cratedb_query_single-groupby-5-1-1
+$GOPATH/bin/tsbs_generate_queries --format cratedb --use-case cpu-only --scale 10 --seed 123 --query-type groupby-orderby-limit --file /tmp/bulk_data/cratedb_query_groupby-orderby-limit
 
 # insert benchmark
 $GOPATH/bin/tsbs_load_cratedb --db-name=benchmark --hosts=localhost --workers=1 --file=/tmp/bulk_data/cratedb_data
--- scripts/run_queries_siridb.sh
+++ scripts/run_queries_siridb.sh
@@ -57,10 +57,10 @@ function run_file()
     cat $FULL_DATA_FILE_NAME \
         | $GUNZIP \
         | $EXE_FILE_NAME \
-            -max-queries $MAX_QUERIES \
-            -workers $NUM_WORKERS \
-            -scale $SCALE \
-            -query-limit $QUERY_LIMIT \
+            --max-queries $MAX_QUERIES \
+            --workers $NUM_WORKERS \
+            --scale $SCALE \
+            --query-limit $QUERY_LIMIT \
         | tee $OUT_FULL_FILE_NAME
 }
 
