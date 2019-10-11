f01be8ecef18ac3179fa58e0e37c308aadf45a9d
Roman Khavronenko <hagen1778@gmail.com>
2019-10-11 20:55:18 +0100

    Make setting flags in scripts compatible with Viper
    Viper does not handle single dash flags the way that Go normally does, so the scripts provided
    needed to be updated to use double dash flags instead.

--- scripts/full_cycle_minitest_clickhouse.sh
+++ scripts/full_cycle_minitest_clickhouse.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-$GOPATH/bin/tsbs_generate_data -format clickhouse -use-case cpu-only -scale 10 -seed 123 -file /tmp/bulk_data/clickhouse_data
+$GOPATH/bin/tsbs_generate_data --format clickhouse --use-case cpu-only --scale 10 --seed 123 --file /tmp/bulk_data/clickhouse_data
 
-$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type lastpoint     -file /tmp/bulk_data/clickhouse_query_lastpoint
-$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type cpu-max-all-1 -file /tmp/bulk_data/clickhouse_query_cpu-max-all-1
-$GOPATH/bin/tsbs_generate_queries -format clickhouse -use-case cpu-only -scale 10 -seed 123 -query-type high-cpu-1    -file /tmp/bulk_data/clickhouse_query_high-cpu-1
+$GOPATH/bin/tsbs_generate_queries --format clickhouse --use-case cpu-only --scale 10 --seed 123 --query-type lastpoint     --file /tmp/bulk_data/clickhouse_query_lastpoint
+$GOPATH/bin/tsbs_generate_queries --format clickhouse --use-case cpu-only --scale 10 --seed 123 --query-type cpu-max-all-1 --file /tmp/bulk_data/clickhouse_query_cpu-max-all-1
+$GOPATH/bin/tsbs_generate_queries --format clickhouse --use-case cpu-only --scale 10 --seed 123 --query-type high-cpu-1    --file /tmp/bulk_data/clickhouse_query_high-cpu-1
 
 $GOPATH/bin/tsbs_load_clickhouse --db-name=benchmark --host=127.0.0.1 --workers=1 --file=/tmp/bulk_data/clickhouse_data
 
