50379344898f57e368d95beaeeb2fd949b704386
Blagoj Atanasovski <blagoj@timescale.com>
2020-03-17 10:46:10 +0100

    Update tsbs_load README to reduce duplicate information
    Also fix a flaky test because order is not guaranteed in
    function that returns array.

--- cmd/tsbs_load/README.md
+++ cmd/tsbs_load/README.md
@@ -4,25 +4,20 @@
   * see available commands and global flags
   * available commands: help, config, load
 * `$ tsbs_load config`
-  * generates an example config file
+  * generates an example config file with default values for each specific target
   * see available flags with `$ tsbs_load config --help`:
     * `--data-source` where to load the data from
-    * `--target` where target db
+    * `--target` where to load data into
     * for valid values execute the command
-    * sample config is generated with default values for each specific target
-* `$ tsbs_load load`
-  * loads data into a target database based on provided config
-  * you need to specify the target database as a sub-command
-  * by default config is loaded from `./config.yaml`
-  * execute `$ tsbs_load load` or `$ tsbs_load load --help` to see available targets
-  and description of flags that are common for all target databases (batch size, target db name, number of workers etc)
-  * **flags overide values in the config.yaml file**
 * `$ tsbs_load load [target]` e.g. `$ tsbs_load load prometheus`
   * loads the data into the target database
   * default config is loaded from `./config.yaml`
   * each property can be overridden by the flags available
   * execute `$tsbs_load load [target] --help` to see target specific flags 
   and their description and default values
+  * execute `$ tsbs_load load` or `$ tsbs_load load --help` to see available targets
+    and description of flags that are common for all target databases (batch size, 
+    target db name, number of workers etc)
   * e.g: `--loader.db-specific.adapter-write-url` overwrites the property 
   in the config file for where is the prometheus adapter listening
-  
+  * **flags overide values in the config.yaml file**
--- cmd/tsbs_load_cratedb/creator_test.go
+++ cmd/tsbs_load_cratedb/creator_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/timescale/tsbs/pkg/data/usecases/common"
-	"reflect"
 	"testing"
 )
 
@@ -10,7 +9,7 @@ func TestDBCreatorReadDataHeader(t *testing.T) {
 	cases := []struct {
 		desc           string
 		input          *common.GeneratedDataHeaders
-		expectedTables []tableDef
+		expectedTables map[string]tableDef
 		expectedToFail bool
 	}{
 		{
@@ -20,8 +19,8 @@ func TestDBCreatorReadDataHeader(t *testing.T) {
 				TagKeys:   []string{"tag1", "tag2"},
 				FieldKeys: map[string][]string{"cpu": {"col1", "col2"}},
 			},
-			expectedTables: []tableDef{
-				{
+			expectedTables: map[string]tableDef{
+				"cpu": {
 					name: "cpu",
 					tags: []string{"tag1", "tag2"},
 					cols: []string{"col1", "col2"},
@@ -34,12 +33,12 @@ func TestDBCreatorReadDataHeader(t *testing.T) {
 				TagKeys:   []string{"tag1", "tag2"},
 				FieldKeys: map[string][]string{"cpu": {"col1", "col2"}, "disk": {"col21", "col22"}},
 			},
-			expectedTables: []tableDef{
-				{
+			expectedTables: map[string]tableDef{
+				"cpu": {
 					name: "cpu",
 					tags: []string{"tag1", "tag2"},
 					cols: []string{"col1", "col2"},
-				}, {
+				}, "disk": {
 					name: "disk",
 					tags: []string{"tag1", "tag2"},
 					cols: []string{"col21", "col22"},
@@ -54,7 +53,7 @@ func TestDBCreatorReadDataHeader(t *testing.T) {
 				FieldKeys: nil,
 			},
 			expectedToFail: false,
-			expectedTables: []tableDef{},
+			expectedTables: map[string]tableDef{},
 		},
 	}
 
@@ -71,21 +70,39 @@ func TestDBCreatorReadDataHeader(t *testing.T) {
 				t.Errorf("%s: incorrect header: %v", c.desc, err)
 			}
 
-			for i, tableDef := range tableDefs {
-				expectedTableDef := c.expectedTables[i]
+			for _, tableDef := range tableDefs {
+				expectedTableDef := c.expectedTables[tableDef.name]
+				if expectedTableDef.name == "" {
+					t.Errorf("expected tables didn't contain returned def %s", tableDef.name)
+				}
 				if tableDef.name != expectedTableDef.name {
 					t.Errorf("%s: incorrect table name: got\n%s\nwant\n%s",
 						c.desc, tableDef.name, expectedTableDef.name)
 				}
-				if !reflect.DeepEqual(tableDef.tags, expectedTableDef.tags) {
+				if !arrEq(tableDef.tags, expectedTableDef.tags) {
 					t.Errorf("%s: incorrect tags: got\n%s\nwant\n%s",
 						c.desc, tableDef.tags, expectedTableDef.tags)
 				}
-				if !reflect.DeepEqual(tableDef.cols, expectedTableDef.cols) {
+				if !arrEq(tableDef.cols, expectedTableDef.cols) {
 					t.Errorf("%s: incorrect cols: got\n%s\nwant\n%s\n",
 						c.desc, tableDef.cols, expectedTableDef.cols)
 				}
 			}
 		}
 	}
+}
+
+func arrEq(a []string, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	if a == nil && b == nil {
+		return true
+	}
+	for i, x := range a {
+		if b[i] != x {
+			return false
+		}
+	}
+	return true
 }
