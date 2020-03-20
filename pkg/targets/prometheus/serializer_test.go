f9875c57da5db34d23ac097255eafd73b113a629
Blagoj Atanasovski <blagoj@timescale.com>
2020-03-20 18:49:55 +0100

    Sort the labels of a data point when converting to TimeSeries

--- pkg/targets/prometheus/serializer_test.go
+++ pkg/targets/prometheus/serializer_test.go
@@ -5,8 +5,11 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/prometheus/prometheus/prompb"
+	"github.com/timescale/tsbs/pkg/data"
 	"github.com/timescale/tsbs/pkg/data/serialize"
+	"sort"
 	"testing"
+	"time"
 )
 
 func TestPrometheusSerializer(t *testing.T) {
@@ -74,5 +77,99 @@ func TestPrometheusSerializer(t *testing.T) {
 func assertEqual(expected, got interface{}, t *testing.T, msg string) {
 	if expected != got {
 		t.Error(fmt.Sprintf("%s. Expected: %v, Got: %v", msg, expected, got))
+	}
+}
+
+func TestConvertToPromSeries(t *testing.T) {
+	now := time.Now()
+	oneFieldPoint := data.NewPoint()
+	oneFieldPoint.SetTimestamp(&now)
+	oneFieldPoint.AppendField([]byte("f"), 1)
+	ofTS := prompb.TimeSeries{
+		Labels:  []prompb.Label{{Name: "__name__", Value: "f"}},
+		Samples: []prompb.Sample{{Value: 1, Timestamp: oneFieldPoint.Timestamp().UnixNano() / 1000000}},
+	}
+
+	twoFieldPoint := data.NewPoint()
+	twoFieldPoint.SetTimestamp(&now)
+	twoFieldPoint.AppendField([]byte("f"), 1)
+	twoFieldPoint.AppendField([]byte("g"), 2)
+	twoFieldPoint.AppendTag([]byte("b"), "t1")
+	twoFieldPoint.AppendTag([]byte("a"), "t2")
+	tfTS1 := prompb.TimeSeries{
+		Labels:  []prompb.Label{{Name: "__name__", Value: "f"}, {Name: "a", Value: "t2"}, {Name: "b", Value: "t1"}},
+		Samples: []prompb.Sample{{Value: 1, Timestamp: twoFieldPoint.Timestamp().UnixNano() / 1000000}},
+	}
+	tfTS2 := prompb.TimeSeries{
+		Labels:  []prompb.Label{{Name: "__name__", Value: "g"}, {Name: "a", Value: "t2"}, {Name: "b", Value: "t1"}},
+		Samples: []prompb.Sample{{Value: 2, Timestamp: twoFieldPoint.Timestamp().UnixNano() / 1000000}},
+	}
+	testCases := []struct {
+		desc      string
+		expError  bool
+		inPoint   *data.Point
+		inBuffer  []prompb.TimeSeries
+		expBuffer []prompb.TimeSeries
+	}{
+		{desc: "Error on wrong size buffer", expError: true, inPoint: oneFieldPoint, inBuffer: []prompb.TimeSeries{}},
+		{
+			desc:      "Single field, single time-series",
+			inPoint:   oneFieldPoint,
+			inBuffer:  make([]prompb.TimeSeries, 1),
+			expBuffer: []prompb.TimeSeries{ofTS},
+		}, {
+			desc:      "Two fields, two time-series, labels sorted",
+			inPoint:   twoFieldPoint,
+			inBuffer:  make([]prompb.TimeSeries, 2),
+			expBuffer: []prompb.TimeSeries{tfTS1, tfTS2},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := convertToPromSeries(tc.inPoint, tc.inBuffer)
+			if tc.expError && err != nil {
+				return
+			} else if tc.expError {
+				t.Errorf("unexpected lack of error")
+				return
+			} else if err != nil {
+				t.Errorf("unexpected error: %v", err)
+				return
+			}
+
+			for i, ts := range tc.expBuffer {
+				returnedTS := tc.inBuffer[i]
+				if len(returnedTS.Samples) != len(ts.Samples) {
+					t.Errorf("wrong samples size; exp: %d; got %d", len(returnedTS.Samples), len(ts.Samples))
+					return
+				}
+				labelsAreSorted := sort.SliceIsSorted(returnedTS.Labels, func(i, j int) bool {
+					return returnedTS.Labels[i].Name < returnedTS.Labels[j].Name
+				})
+				if !labelsAreSorted {
+					t.Errorf("labels not sorted by name: %v", returnedTS.Labels)
+				}
+				for j, retLabel := range returnedTS.Labels {
+					expLabel := tc.expBuffer[i].Labels[j]
+					if expLabel.Name != retLabel.Name {
+						t.Errorf("labels nor ordered; exp: %s; got %s", expLabel.Name, retLabel.Name)
+					}
+					if expLabel.Value != retLabel.Value {
+						t.Errorf("label value missmatch; exp: %s; got %s", expLabel.Value, retLabel.Value)
+					}
+				}
+				for j, retSample := range returnedTS.Samples {
+					expSample := tc.expBuffer[i].Samples[j]
+					if retSample.Value != expSample.Value {
+						t.Errorf("sample value missmatch; exp: %f ; got: %f", expSample.Value, retSample.Value)
+					}
+					if retSample.Timestamp != expSample.Timestamp {
+						t.Errorf("sample time missmatch; exp: %d; got: %d", expSample.Timestamp, retSample.Timestamp)
+					}
+
+				}
+			}
+		})
 	}
 }
