337ebd28400631151ad4a19e118027567de81ea0
Blagoj Atanasovski <blagoj@timescale.com>
2020-10-19 13:38:30 +0200

    Update VictoriaMetrics code to require to pkg/query

--- cmd/tsbs_generate_queries/databases/victoriametrics/common.go
+++ cmd/tsbs_generate_queries/databases/victoriametrics/common.go
@@ -9,7 +9,7 @@ import (
 	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/uses/devops"
 	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/utils"
 	iutils "github.com/timescale/tsbs/internal/utils"
-	"github.com/timescale/tsbs/query"
+	"github.com/timescale/tsbs/pkg/query"
 )
 
 type BaseGenerator struct{}
