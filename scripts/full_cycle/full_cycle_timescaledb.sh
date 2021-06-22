6c2c473d3a83ec3087810b61956a1009b0393ac1
Jônatas Davi Paganini <jonatasdp@gmail.com>
2021-06-22 16:14:39 -0300

    Extract variables and loop commands

--- scripts/full_cycle/full_cycle_timescaledb.sh
+++ scripts/full_cycle/full_cycle_timescaledb.sh
@@ -1,27 +1,74 @@
-#!/bin/bash
+#!/bin/bash -x
 # showcases the ftsb 3 phases for timescaledb
 # - 1) data and query generation
 # - 2) data loading/insertion
 # - 3) query execution
 
-MAX_RPS=${MAX_RPS:-"0"}
-MAX_QUERIES=${MAX_QUERIES:-"1000"}
+# Auth
+HOST=127.0.0.1
+PORT=5432
+USER=postgres
 PASSWORD=${PASSWORD:-""}
+DATABASE_NAME=${DATABASE_NAME:-"benchmark"}
 
-mkdir -p /tmp/bulk_data
+# Setup
+USE_CASE=${USE_CASE:-"cpu-only"}
+QUERY_TYPES=(lastpoint cpu-max-all-1 high-cpu-1)
+SCALE=10
+SEED=123
+WORKERS=1
+MAX_QUERIES=${MAX_QUERIES:-"1000"}
+
+# Folders setup
+
+TARGET_DATA_FOLDER=${TARGET_DATA_FOLDER:-"/tmp/bulk_data"}
+DATA_FILE=$TARGET_DATA_FOLDER/timescaledb_data
+mkdir -p $TARGET_DATA_FOLDER
+
 
 # generate queries
-$GOPATH/bin/tsbs_generate_data --format timescaledb --use-case cpu-only --scale 10 --seed 123 --file /tmp/bulk_data/timescaledb_data
+$GOPATH/bin/tsbs_generate_data \
+    --format timescaledb \
+    --use-case $USE_CASE \
+    --scale $SCALE \
+    --seed $SEED \
+    --file $DATA_FILE
 
-# generate queries
-$GOPATH/bin/tsbs_generate_queries --queries=${MAX_QUERIES} --format timescaledb --use-case cpu-only --scale 10 --seed 123 --query-type lastpoint     --file /tmp/bulk_data/timescaledb_query_lastpoint
-$GOPATH/bin/tsbs_generate_queries --queries=${MAX_QUERIES} --format timescaledb --use-case cpu-only --scale 10 --seed 123 --query-type cpu-max-all-1 --file /tmp/bulk_data/timescaledb_query_cpu-max-all-1
-$GOPATH/bin/tsbs_generate_queries --queries=${MAX_QUERIES} --format timescaledb --use-case cpu-only --scale 10 --seed 123 --query-type high-cpu-1    --file /tmp/bulk_data/timescaledb_query_high-cpu-1
+for _type in "${QUERY_TYPES[@]}"
+do
+   :
+  $GOPATH/bin/tsbs_generate_queries \
+    --format timescaledb \
+    --queries ${MAX_QUERIES} \
+    --use-case $USE_CASE \
+    --scale $SCALE \
+    --seed $SEED \
+    --query-type $_type \
+    --file $TARGET_DATA_FOLDER/timescaledb_query_$_type
+done
 
 # insert benchmark
-$GOPATH/bin/tsbs_load_timescaledb --pass=${PASSWORD} --postgres="sslmode=disable port=5433" --db-name=benchmark --host=127.0.0.1 --user=postgres --workers=1 --file=/tmp/bulk_data/timescaledb_data
+$GOPATH/bin/tsbs_load_timescaledb \
+    --pass=${PASSWORD} \
+    --postgres="sslmode=disable port=${PORT}" \
+    --db-name=${DATABASE_NAME} \
+    --host=${HOST} \
+    --user=${USER} \
+    --workers=${WORKERS} \
+    --file=${DATA_FILE}
 
-# queries benchmark
-$GOPATH/bin/tsbs_run_queries_timescaledb --max-rps=${MAX_RPS} --hdr-latencies="${MAX_RPS}rps_timescaledb_query_lastpoint.hdr" --pass=${PASSWORD} --postgres="sslmode=disable port=5433" --db-name=benchmark --hosts=127.0.0.1 --user=postgres --workers=1 --max-queries=${MAX_QUERIES} --file=/tmp/bulk_data/timescaledb_query_lastpoint
-$GOPATH/bin/tsbs_run_queries_timescaledb --max-rps=${MAX_RPS} --hdr-latencies="${MAX_RPS}rps_timescaledb_query_cpu-max-all-1.hdr" --pass=${PASSWORD} --postgres="sslmode=disable port=5433" --db-name=benchmark --hosts=127.0.0.1 --user=postgres --workers=1 --max-queries=${MAX_QUERIES} --file=/tmp/bulk_data/timescaledb_query_cpu-max-all-1
-$GOPATH/bin/tsbs_run_queries_timescaledb --max-rps=${MAX_RPS} --hdr-latencies="${MAX_RPS}rps_timescaledb_query_high-cpu-1.hdr" --pass=${PASSWORD} --postgres="sslmode=disable port=5433" --db-name=benchmark --hosts=127.0.0.1 --user=postgres --workers=1 --max-queries=${MAX_QUERIES} --file=/tmp/bulk_data/timescaledb_query_high-cpu-1
+
+for _type in "${QUERY_TYPES[@]}"
+do
+   :
+    # queries benchmark
+    $GOPATH/bin/tsbs_run_queries_timescaledb \
+        --max-queries=${MAX_QUERIES} \
+        --pass=${PASSWORD} \
+        --postgres="sslmode=disable port=${PORT}" \
+        --db-name=${DATABASE_NAME} \
+        --hosts=${HOST} \
+        --user=${USER} \
+        --workers=${WORKERS} \
+        --file=$TARGET_DATA_FOLDER/timescaledb_query_$_type
+done
