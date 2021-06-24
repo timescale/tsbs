2a05c5c6e5d6f04ad8218e42c1cebf903f4d5c78
Ryan Booz <ryan@timescale.com>
2021-06-24 12:36:13 -0400

    Adding options for TimescaleDB input

--- pkg/targets/timescaledb/implemented_target.go
+++ pkg/targets/timescaledb/implemented_target.go
@@ -1,13 +1,14 @@
 package timescaledb
 
 import (
+	"time"
+
 	"github.com/blagojts/viper"
 	"github.com/spf13/pflag"
 	"github.com/timescale/tsbs/pkg/data/serialize"
 	"github.com/timescale/tsbs/pkg/data/source"
 	"github.com/timescale/tsbs/pkg/targets"
 	"github.com/timescale/tsbs/pkg/targets/constants"
-	"time"
 )
 
 func NewTarget() targets.ImplementedTarget {
@@ -51,6 +52,7 @@ func (t *timescaleTarget) TargetSpecificFlags(flagPrefix string, flagSet *pflag.
 	flagSet.Bool(flagPrefix+"use-jsonb-tags", false, "Whether tags should be stored as JSONB (instead of a separate table with schema)")
 	flagSet.Bool(flagPrefix+"in-table-partition-tag", false, "Whether the partition key (e.g. hostname) should also be in the metrics hypertable")
 
+	flagSet.Int(flagPrefix+"replication-factor", 0, "Setting replication factor >= 1 will create a distributed hypertable")
 	flagSet.Int(flagPrefix+"partitions", 0, "Number of partitions")
 	flagSet.Duration(flagPrefix+"chunk-time", 12*time.Hour, "Duration that each chunk should represent, e.g., 12h")
 
@@ -64,5 +66,6 @@ func (t *timescaleTarget) TargetSpecificFlags(flagPrefix string, flagSet *pflag.
 	flagSet.String(flagPrefix+"write-replication-stats", "", "File to output replication stats to")
 	flagSet.Bool(flagPrefix+"create-metrics-table", true, "Drops existing and creates new metrics table. Can be used for both regular and hypertable")
 
+	flagSet.Bool(flagPrefix+"use-insert", false, "Provides the option to test data inserts with batched INSERT commands rather than the preferred COPY function")
 	flagSet.Bool(flagPrefix+"force-text-format", false, "Send/receive data in text format")
 }
