6f67d9e4e7a21f3788c848dc223cec48c1160609
hu,mingming <humingmingak@qq.com>
2022-04-18 14:32:25 +0800

    repair bug: when load normal data to questdb there have already cpu table,report error,remove redundant check

--- cmd/tsbs_load_questdb/creator.go
+++ cmd/tsbs_load_questdb/creator.go
@@ -20,20 +20,10 @@ func (d *dbCreator) Init() {
 }
 
 func (d *dbCreator) DBExists(dbName string) bool {
-	r, err := execQuery(questdbRESTEndPoint, "SHOW TABLES")
+	_, err := execQuery(questdbRESTEndPoint, "SHOW TABLES")
 	if err != nil {
 		panic(fmt.Errorf("fatal error, failed to query questdb: %s", err))
 	}
-	for i, v := range r.Dataset {
-		if i >= 0 && v[0] == "cpu" {
-			panic(fmt.Errorf("fatal error, cpu table already exists"))
-		}
-	}
-	// Create minimal table with o3 params
-	//        r, err = execQuery(questdbRESTEndPoint, "CREATE TABLE cpu (hostname SYMBOL, region SYMBOL, datacenter SYMBOL, rack SYMBOL, os SYMBOL, arch SYMBOL, team SYMBOL, service SYMBOL, service_version SYMBOL, service_environment SYMBOL, usage_user LONG, usage_system LONG, usage_idle LONG, usage_nice LONG, usage_iowait LONG, usage_irq LONG, usage_softirq LONG, usage_steal LONG, usage_guest LONG, usage_guest_nice LONG, timestamp TIMESTAMP) timestamp(timestamp) PARTITION BY DAY WITH o3MaxUncommittedRows=500000, o3CommitHysteresis=300s")
-	//        if err != nil {
-	//          panic(fmt.Errorf("fatal error, failed to create cpu table: %s", err))
-	//        }
 
 	return false
 }
