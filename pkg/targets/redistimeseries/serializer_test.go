1af2981f8f0c7882885055968ab71aa06cd9e886
filipecosta90 <filipecosta.90@gmail.com>
2022-06-29 16:02:07 +0100

    Fixed TestRedisTimeSeriesSerializerSerialize() outputs

--- pkg/targets/redistimeseries/serializer_test.go
+++ pkg/targets/redistimeseries/serializer_test.go
@@ -11,7 +11,7 @@ func TestRedisTimeSeriesSerializerSerialize(t *testing.T) {
 		{
 			Desc:       "a regular Point",
 			InputPoint: serialize.TestPointDefault(),
-			Output:     "7116 TS.CREATE {host_0}_usage_guest_nice LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname usage_guest_nice\n7116 TS.MADD {host_0}_usage_guest_nice 1451606400000 38.24311829\n",
+			Output:     "7116 TS.CREATE {host_0}_cpu_usage_guest_nice LABELS hostname host_0 region eu-west-1 datacenter eu-west-1b measurement cpu fieldname usage_guest_nice\n7116 TS.MADD {host_0}_cpu_usage_guest_nice 1451606400000 38.24311829\n",
 		},
 	}
 
