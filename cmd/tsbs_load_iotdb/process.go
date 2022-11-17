9c99cd9215cb273736f372aca028185d01839871
citrusreticulata <1372722289@qq.com>
2022-11-17 12:09:07 +0800

    optimize ProcessBatch function
    optimize ProcessBatch function
    using records struct to simplify code

--- cmd/tsbs_load_iotdb/process.go
+++ cmd/tsbs_load_iotdb/process.go
@@ -28,69 +28,62 @@ func (p *processor) Init(numWorker int, doLoad, _ bool) {
 	p.ProcessedTagsDeviceIDMap = make(map[string]bool, 1024)
 }
 
+type records struct {
+	deviceId     []string
+	measurements [][]string
+	dataTypes    [][]client.TSDataType
+	values       [][]interface{}
+	timestamps   []int64
+}
+
+func (p *processor) pointsToRecords(points []*iotdbPoint) (records, []string) {
+	var rcds records
+	var sqlList []string
+	for _, row := range points {
+		rcds.deviceId = append(rcds.deviceId, row.deviceID)
+		rcds.measurements = append(rcds.measurements, row.measurements)
+		rcds.dataTypes = append(rcds.dataTypes, row.dataTypes)
+		rcds.values = append(rcds.values, row.values)
+		rcds.timestamps = append(rcds.timestamps, row.timestamp)
+		_, exist := p.ProcessedTagsDeviceIDMap[row.deviceID]
+		if !exist {
+			sqlList = append(sqlList, row.generateTagsAttributesSQL())
+			p.ProcessedTagsDeviceIDMap[row.deviceID] = true
+		}
+	}
+	return rcds, sqlList
+}
+
+func minInt(x int, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func (p *processor) ProcessBatch(b targets.Batch, doLoad bool) (metricCount, rowCount uint64) {
 	batch := b.(*iotdbBatch)
 
 	// Write records
 	if doLoad {
 		var sqlList []string
-		if p.recordsMaxRows > 0 {
-			for index := 0; index < len(batch.points); {
-				var (
-					deviceId     []string
-					measurements [][]string
-					dataTypes    [][]client.TSDataType
-					values       [][]interface{}
-					timestamps   []int64
-				)
-				for thisRecordsCnt := 0; thisRecordsCnt < recordsMaxRows && index < len(batch.points); {
-					row := batch.points[index]
-					deviceId = append(deviceId, row.deviceID)
-					measurements = append(measurements, row.measurements)
-					dataTypes = append(dataTypes, row.dataTypes)
-					values = append(values, row.values)
-					timestamps = append(timestamps, row.timestamp)
-					_, exist := p.ProcessedTagsDeviceIDMap[row.deviceID]
-					if !exist {
-						sqlList = append(sqlList, row.generateTagsAttributesSQL())
-						p.ProcessedTagsDeviceIDMap[row.deviceID] = true
-					}
-					thisRecordsCnt++
-					index++
-				}
-				_, err := p.session.InsertRecords(
-					deviceId, measurements, dataTypes, values, timestamps,
-				)
-				if err != nil {
-					fatal("ProcessBatch error:%v", err)
-				}
+		for index := 0; index < len(batch.points); {
+			startIndex := index
+			var endIndex int
+			if p.recordsMaxRows > 0 {
+				endIndex = minInt(len(batch.points), index+p.recordsMaxRows)
+			} else {
+				endIndex = len(batch.points)
 			}
-		} else {
-			var (
-				deviceId     []string
-				measurements [][]string
-				dataTypes    [][]client.TSDataType
-				values       [][]interface{}
-				timestamps   []int64
-			)
-			for _, row := range batch.points {
-				deviceId = append(deviceId, row.deviceID)
-				measurements = append(measurements, row.measurements)
-				dataTypes = append(dataTypes, row.dataTypes)
-				values = append(values, row.values)
-				timestamps = append(timestamps, row.timestamp)
-				_, exist := p.ProcessedTagsDeviceIDMap[row.deviceID]
-				if !exist {
-					sqlList = append(sqlList, row.generateTagsAttributesSQL())
-					p.ProcessedTagsDeviceIDMap[row.deviceID] = true
-				}
-			}
+			rcds, tempSqlList := p.pointsToRecords(batch.points[startIndex:endIndex])
+			sqlList = append(sqlList, tempSqlList...)
 			_, err := p.session.InsertRecords(
-				deviceId, measurements, dataTypes, values, timestamps,
+				rcds.deviceId, rcds.measurements, rcds.dataTypes, rcds.values, rcds.timestamps,
 			)
 			if err != nil {
 				fatal("ProcessBatch error:%v", err)
 			}
+			index = endIndex
 		}
 		// handle create timeseries SQL to insert tags
 		for _, sql := range sqlList {
